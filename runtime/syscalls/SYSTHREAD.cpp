3293ffaa9b1344fd30773c1376c9ed3bfeda3773
secondborn-fromDeath <laymanvonton@gmail.com>
2024-07-13 04:19:22 +0000

    Create SYSTHREAD.cpp

---
+++ runtime/syscalls/SYSTHREAD.cpp
@@ -0,0 +1,30 @@
+//on the tin it says that you are cloning everything (at least until we have flags, TODO) from the calling thread...
+void thread(){
+	Kingthread * ktrd; get_kingthread_object(ktrd);
+	ustd_t processor_id = stream_init(kings.THREAD);
+	ulong_t thread_index = ktrd->pool_alloc(1);
+	ktrd->calendar[processor_id] = 0;
+
+	Thread * calling_thread = get_thread_object();
+	memcpy(ktrd->pool[thread_index], calling_thread, sizeof(Thread));
+
+	Kingptr * kptr; get_pointer_object(kptr);
+	stream_init(Kingptr);
+	ktrd->pool[thread_index].desc_indexes = kptr->pool_alloc(calling_thread->desc_count);
+	kptr->pool[processor_id] = 0;
+	memcpy(ktrd->pool[thread_index].desc_indexes,calling_thread->desc_indexes,calling_thread->desc_count);
+
+	//making a stack for the new thread
+	Process * process = calling_thread->parent;
+	void * treebackup = mm->vm_ram_table;
+	mm->vm_ram_table = process->pagetree;
+	void * entry = mm->vmtree_fetch(header->stacksize+1,pag.SMALLPAGE);
+	ktrd->pool[thread_index]->state.stack_ptr = entry;
+	entry = mm->vmto_entry(entry,&pagetype) + (header->stacksize+1)*8;
+	for (ustd_t h = 0; h < header->stacksize; ++h){				//NOTE HARDENING
+	    void * hold = mm->getphys_identity(1,pag.SMALLPAGE);
+	    entry* = mm->memreq_template(pag.SMALLPAGE,MAP,WRITEBACK) | hold<<12;
+	    entry -= 8;
+	}
+	entry* = 0;
+}
