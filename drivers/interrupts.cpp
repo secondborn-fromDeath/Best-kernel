d9ca9d9dbbeb72b7d2ee618e475b0c70084c84c5
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-20 21:29:47 +0000

    Update interrupts.cpp

--- drivers/interrupts.cpp
+++ drivers/interrupts.cpp
@@ -2,7 +2,7 @@
 	This is a driver for intel's IO apics, this file functions as the source for information along with the
 	multiprocessor specification and the implementation for the Loonix kernel
 
-	IO_apic portion (these are pin-programming functions):
+	IOapic portion (these are pin-programming functions):
 		vector assignment
 		masking, delivery mode,
 		delivery status and sync, trigger mode
@@ -26,13 +26,13 @@ page 858 of the i486 manual
 		IO apic portion of things:
 	*/
 
-Class IO_apic{
+Class IOapic{
 	void * mmio;
 	ustd_t pinsnum;
 };
 
 	//you can get the information on the ir pins of devices from pci
-	ustd_t assign_vector(IO_apic * info, ustd_t rel_pin, uchar_t vector){
+	ustd_t assign_vector(IOapic * info, ustd_t rel_pin, uchar_t vector){
 		if !(info->pinsnum > rel_pin){ return 1;}
 		ustd_t * ioapic_cfg = info->mmio;		//the "default" is FEC0000.
 		//writing to regsel the pin number
@@ -43,14 +43,14 @@ Class IO_apic{
 		ioapic_cfg[1] = nyu;
 		return 0;
 	}
-	ustd_t get_vector(IO_apic * info, ustd_t rel_pin){
+	ustd_t get_vector(IOapic * info, ustd_t rel_pin){
 		if !(info->pinsnum > rel_pin){ return 1;}
 		ustd_t * ioapic_cfg = info->mmio;		//FEC00000 + 4096*ioapic_id
 		ioapic_cfg[0] = 16+rel_pin;
 		ustd_t nyu = ioapic_cfg[1];
 		return (char)nyu;
 	}
-	ustd_t mask_pin(IO_apic * info, ustd_t rel_pin){
+	ustd_t mask_pin(IOapic * info, ustd_t rel_pin){
 		if !(info->pinsnum > rel_pin){ return 1;}
 		ustd_t * ioapic_cfg = info->mmio;
 		//pinnum into regsel
@@ -61,7 +61,7 @@ Class IO_apic{
 		ioapic_cfg[1] = nyu;
 		return 0;
 	}
-	ustd_t unmask_pin(IO_apic * info, ustd_t rel_pin){
+	ustd_t unmask_pin(IOapic * info, ustd_t rel_pin){
 		if !(info->pinsnum > rel_pin){ return 1;}
 		ustd_t * ioapic_cfg = info->mmio;
 		//pinnum into regsel
@@ -73,7 +73,7 @@ Class IO_apic{
 		return 0;
 	}
 	enum delivmod{ FIXED,LOWPRIO,RESONE,RESTWO,NMI,RESET,RESTHREE,EXTINT};
-	ustd_t set_delivery_mode(IO_apic * info, ustd_t rel_pin, ustd_t mode){
+	ustd_t set_delivery_mode(IOapic * info, ustd_t rel_pin, ustd_t mode){
 		if (mode > delivmod.EXTINT){ return 1;}
 		if !(info->pinsnum > rel_pin){ return 1;}
 		//you know the drill
@@ -84,7 +84,7 @@ Class IO_apic{
 		ioapic_cfg)[1] = nyu;
 		return 0;
 	}
-	ustd_t get_delivery_mode(IO_apic * info, ustd_t rel_pin){
+	ustd_t get_delivery_mode(IOapic * info, ustd_t rel_pin){
 		if !(info->pinsnum > rel_pin){ return 32;}
 		ustd_t * ioapic_cfg = info->mmio;
 		ioapic_cfg[0] = 16+rel_pin;
@@ -97,7 +97,7 @@ Class IO_apic{
 		Same goes for the remote IRR
 	*/
 	enum trigmode{ EDGE,LEVEL};
-	ustd_t set_trigger_mode(IO_apic * info, ustd_t rel_pin, ustd_t trigger_mode){
+	ustd_t set_trigger_mode(IOapic * info, ustd_t rel_pin, ustd_t trigger_mode){
 		if (trigger_mode > trigmode.LEVEL){ return 1;}
 		if !(info->pinsnum > rel_pin){ return 1;}
 		ustd_t * ioapic_cfg = info->mmio;
@@ -105,7 +105,7 @@ Class IO_apic{
 		ustd_t nyu = ioapic_cfg[1];
 		return nyu<<;
 	}
-	ustd_t get_trigger_mode(IO_apic * info, ustd_t rel_pin){
+	ustd_t get_trigger_mode(IOapic * info, ustd_t rel_pin){
 		if (trigger_mode > trigmode.LEVEL){ return 1;}
 		if !(info->pinsnum > rel_pin){ return 1;}
 		ustd_t * ioapic_cfg = info->mmio;
@@ -123,19 +123,18 @@ Class IO_apic{
 	*/
 	ustd_t set_task_priority(ustd_t prio){
 		if (prio > 15){ return 1;}
-		ustd_t * lapic = 0xFEE00000;
-		lapic[0x80/4] = prio;
+		((ustd_t *)0xFEE00080)* = prio;
 		return 0;
 	}
 	//no need for get
-	void signal_EOI(){
+	void signal_EOI(void){
 		((ustd_t *)0xFEE000B0)* = 1;
 	}
 	ustd_t set_spurious(uchar_t newvec){
 		((ustd_t *)0xFEE000F0)* = (ustd_t)newvec;
 		return 0;
 	}
-	ustd_t get_spurious(){
+	ustd_t get_spurious(void){
 		return ((ustd_t *)0xFEE000F0)*;
 	}
 	/*
@@ -144,29 +143,9 @@ Class IO_apic{
 	*/
 	ustd_t remote_read(Kontrol * status, ustd_t remote_id, ustd_t regnum){
 		ustd_t * lapic = 0xFEE00000;
-		switch (status->lapic_version){
-			case LEGACY:{
-				//destination
-				lapic[0x300/4] = remote_id<<56;
-				//vector
-				ustd_t g = lapic[0x310/4];
-				(char)g = regnum;
-				lapic[0x310/4] = g;
-			break;}
-			case X2APIC:{
-				ustd_t a;
-				ustd_t c = 0x830;
-				ustd_t dx;
-				__asm__ volatile(
-				"rdmsr"
-				"or %4,%%eax\n\t"	//i havent read the C abi idk what that clobbers to
-				"mov %%ebx,%%edx\n\t"
-				"shl $32,%%edx\n\t"	//DANGER might not be 32bit
-				"wrmsr"
-				::"r"(a),"r"(c),"r"(dx),"r"(remote_id),"r"(regnum)
-				);
-			break;}
-		}
+		lapic[0x310/4] = brother_id<<24;
+		ustd_t interrupt_mask = get_targetipi_mask(void) | regnum;
+		((char *)lapic)[0x300/8] = regnum;
 		return lapic[0xC0/4];
 	}
 	/*
@@ -186,3 +165,44 @@ Class IO_apic{
 		}
 		lapic[0x320/4] = pipe;
 	}
+	ustd_t get_targetipi_mask(void){
+		return 1<<14 | 1<<15 | 0<<18;		//assert, level triggered, target in +0x310
+	}
+	ustd_t get_selfipi_mask(void){
+		return 1<<14 | 1<<15 | 1<<18;		//only self
+	}
+	ustd_t get_allipi_mask(void){
+		return 1<<14 | 1<<15 | 2<<18;		//all
+	}
+	ustd_t get_exselfipi_mask(void){
+		return 1<<14 | 1<<15 | 3<<18;		//all excluding self
+	}
+	/*
+	Puts all other processors to HLT
+	*/
+	void brothers_sleep(void){
+		ustd_t interrupt_mask = get_exselfipi_mask(void);
+		((ustd_t *)0xFEE0300)* = interrupt_mask | ints.SLEEP;   //sent
+	}
+	void brothers_wake(void){
+		ustd_t interrupt_mask = get_exselfipi_mask(void);
+		((ustd_t *)0xFEE0300)* = interrupt_mask | ints.WAKE;
+	}
+	void wake_brother(ustd_t brother_id){
+		((ustd_t *)0xFEE0310)* = brother_id<<56;
+		ustd_t interrupt_mask = get_targetipi_mask(void);
+		((ustd_t *)0xFEE0300)* = interrupt_mask | ints.SLEEP;
+	}
+	void sleep_brother(ustd_t processor_id){
+		((ustd_t *)0xFEE0310)* = brother_id<<56;
+		ustd_t interrupt_mask = get_targetipi_mask(void);
+		((ustd_t *)0xFEE0300)* = interrupt_mask | ints.WAKE;
+	}
+	void nap(void){
+		((ustd_t *)0xFEE0310)* = brother_id<<56;
+		ustd_t interrupt_mask = get_selfipi_mask(void);
+		((ustd_t *)0xFEE0300)* = interrupt_mask | ints.SLEEP;
+	}
+	//these are the routines
+        void memreq_down_handler(void){__asm__("HLT\n\t");}
+        void memreq_up_handler(void){__asm__("add $24,%%rsp\n\t""iret\n\t");}   //ss,cs,ip,sp,rflags	skipping the new stack and returning to the task we procrastinated
