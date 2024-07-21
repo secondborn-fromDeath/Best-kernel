be1c3b14934bdf9a224a243896d16aadeab95905
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-21 22:46:26 +0000

    Update and rename 32bit_start.cpp to nothing.one
    so yeah i found out efi boots you into 64bit

--- init/32bit_start.cpp
+++
@@ -1,9 +0,0 @@
-//NOTE compile -m32
-
-void 32bit_start(void * imagehandle, void * efisystab){		//DANGER gcc pussy clobbers might mess the cpu mode up (should be fine though because control register changes shouldnt take effect until the end of the instruction pipeline, the swithces have to become macros...)
-	auto * func = &64bit_start;
-	mswitch_32to64(void);
-	__asm__(
-		"JMP %0\n\t"
-		::"r"(func):);
-}
