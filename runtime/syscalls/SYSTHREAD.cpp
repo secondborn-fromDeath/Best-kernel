20c433acc231a5c33a883aa99b5687c2baca0451
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-21 22:42:51 +0000

    Update SYSTHREAD.cpp

--- runtime/syscalls/SYSTHREAD.cpp
+++ runtime/syscalls/SYSTHREAD.cpp
@@ -1,31 +1,31 @@
 //on the tin it says that you are cloning everything (at least until we have flags, TODO) from the calling thread...
-void thread(){
-	Kingthread * ktrd; get_kingthread_object(ktrd);
-	ustd_t processor_id = ktrd->stream_init(void);
-	ulong_t thread_index = ktrd->pool_alloc(1);
-	__non_temporal ktrd->calendar[processor_id] = 0;
+void thread(void){
+	Kingthread * ktrd = get_kingthread_object(void);
+	ktrd->stream_init(void);
+	Thread * newfag = ktrd->pool_alloc(1);
+	__non_temporal ktrd->calendar = 0;
 
 	Thread * calling_thread = get_thread_object(void);
-	memcpy(ktrd->pool[thread_index], calling_thread, sizeof(Thread));
+	memcpy(newfag,calling_thread,sizeof(Thread));
+	newfag->taken = 0;					//newfriend is not being taken care of by anybody unlike oldfren
 
-	Kingptr * kptr; get_pointer_object(kptr);
+	Kingptr * kptr = get_pointer_object(void);
 	kptr->stream_init(void);
-	ktrd->pool[thread_index].desc_indexes = kptr->pool_alloc(calling_thread->desc_count);
-	kptr->pool[processor_id] = 0;
-	__non_temporal kptr->calendar[processor_id] = 0;
-	memcpy(ktrd->pool[thread_index].desc_indexes,calling_thread->desc_indexes,calling_thread->desc_count);
+	newfag->desc_indexes = kptr->pool_alloc(calling_thread->desc_count);
+	__non_temporal kptr->calendar = 0;
+	memcpy(newfag.desc_indexes,calling_thread->desc_indexes,calling_thread->desc_count);
 
 	//making a stack for the new thread
 	Process * process = calling_thread->parent;
 	void * treebackup = mm->vm_ram_table;
 	mm->vm_ram_table = process->pagetree;
 	void * entry = mm->vmtree_fetch(header->stacksize+1,pag.SMALLPAGE);
-	ktrd->pool[thread_index]->state.stack_ptr = entry;
+	newfag->state.stack_ptr = (entry*)<<5>>18<<1;
 	entry = mm->vmto_entry(entry,&pagetype) + (header->stacksize+1)*8;
 	for (ustd_t h = 0; h < header->stacksize; ++h){				//NOTE HARDENING
 	    void * hold = mm->getphys_identity(1,pag.SMALLPAGE);
 	    entry* = mm->memreq_template(pag.SMALLPAGE,MAP,WRITEBACK) | hold<<12;
 	    entry -= 8;
 	}
-	entry* = 0;
+	entry* = 0;	//guard page
 }
