a11bf30f119db098e94d20f2f8e53f0ef506b016
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-21 22:42:16 +0000

    Update SYSEXEC.cpp

--- runtime/syscalls/SYSEXEC.cpp
+++ runtime/syscalls/SYSEXEC.cpp
@@ -13,49 +13,57 @@ Class Exec_header{
 	ulong_t checksum;
 };
 
+//The checksum is valid if subtracting it to the sum of the other fields ends up being 0
+#define EXEC_CHECK_CHECKSUM{	\
+if !(header->heapsize+header->tls_size+ header->stacksize+header->max_descriptors+header->start+header->expected_memory_usage+header->program_type+	\
+((ustd_t *)header->id)[0]+((ustd_t *)header->id)[8]+((ustd_t *)header->id)[16]+((ustd_t *)header->id)[24] == header->checksum)	\
+}
+
 
 /*this does not replace the calling process like oonix and instead simply makes a new one
 	Reading the header into a page, parsing it (evaluating the checksum really)
 	reserving the pages for the executable, making the process structure and initializing it,
 	allocating stuff into the process' pagetree, setting the main thread's ip and sp
 
-NOTE this assumes that you never do pageswapping (see entire executable being loaded), with competent users and developers this is fine but
-if the game engine is shit (You?... I? We maybe. Are the incompetent developers!!! ???) and you cant fix it then you will need to come back here.
+NOTE DANGER MOST IMPORTANT PLEASE READ IT FUCK!
+						remember that this heavily affects boot, setup_drivers()...
 */
-ustd_t exec(ustd_t findex){
-	Kingmem * mm; get_kingmem_object(mm);
-	Virtual_fs * vfs; get_vfs_object(vfs);
-	DisksKing * dking; get_disksking_object(dking);
+//returns the index into the processes array
+ulong_t exec(ustd_t findex){
+	Kingmem * mm = get_kingmem_object(void);
+	Virtual_fs * vfs = get_vfs_object(void);
+	DisksKing * dking = get_disksking_object(void);
 
-	ustd_t processor_id = mm->stream_init(void);
+	Storage * source = &vfs->descriptions[findex];
+
+	mm->stream_init(void);
 	dking->stream_init(void);
+	Exec_header * header = vfs->load_page(dking,source,0);
 
-	Exec_header * header = mm->reserve_phys_identity(1,pag.SMALLPAGE);
-	dking->read(vfs->descriptions[findex]->disk,vfs->descriptions[findex]->diskpos,header,1,pag.SMALLPAGE);	//mapping only the header so the system doesnt crash opening something like a gigabyte large video file
-	//The checksum is valid if subtracting it to the sum of the other fields ends up being 0
-	if !(header->heapsize+header->tls_size+ header->stacksize+header->max_descriptors+header->start+header->expected_memory_usage+header->program_type+	\
-	((ustd_t *)header->id)[0]+((ustd_t *)header->id)[8]+((ustd_t *)header->id)[16]+((ustd_t *)header->id)[24] == header->checksum){ return 1;}
-
-	Thread * calling_thread = get_thread_object();
+	EXEC_CHECK_CHECKSUM{
+		//NOTE HARDENING CLEANUP ROUTINE
+		return -1;
+	}
+	Thread * calling_thread = get_thread_object(void);
 	Process * calling_process = calling_thread->parent;
-	if (header_program_type == programtypes.SYSTEM) && (calling_process-<owner_id != users.ROOT){ return 2;}
+	if (header->program_type == programtypes.SYSTEM) && (calling_process->owner_id != users.ROOT){ return -2;}	//this is nonsense lol
 
-	void * executable[vfs->descriptions[findex].meta.length];
-	executable[0] = header;
+	source->shared_contents[0] = header;
+	ustd_t multi = get_multi_from_pagetype(source->mapped_pagetype);
 	for (ustd_t i = 1; i < vfs->descriptions[findex].meta.length; ++i){
-		executable[i] = mm->reserve_phys_identity(mm,1,SMALLPAGE);
-		dking->read(vfs->descriptions[findex]->disk,vfs->descriptions[findex]->diskpos+i*4096,header,1,pag.SMALLPAGE);
+		vfs->load_page(dking,source,i);
 	}
 
-	__non_temporal dking->calendar[processor_id] = 0;
-	__non_temporal mm->calendar[processor_id] = 0;
+	__non_temporal dking->calendar = 0;
+	__non_temporal mm->calendar = 0;
 
-	Kingprocess * kprc; get_kingprocess_object(kprc);
+
+	Kingprocess * kprc = get_kingprocess_object(void);
 	stream_init(kings.PROCESS);
-	ulong_t process_index = kprc->pool_alloc(1);
-	__non_temporal kprc->calendar[processor_id] = 0;
+	Process * process = kprc->pool_alloc(1);
+	__non_temporal kprc->calendar = 0;
 
-	kprc->pool[process_index] = {
+	process* = {
 //		.pid = process_index,
 		.owner_id = calling_process->owner_id,		//inherited from the launching shell
 		.sigmask = 0,
@@ -64,24 +72,24 @@ ustd_t exec(ustd_t findex){
 		.max_desc = header->max_descriptors,
 	};
 
-	Kingptr * ptr; get_pointer_object(ptr);
+	Kingptr * ptr = get_pointer_object(void);
 	stream_init(kings.POINTERS);
-	kprc->pool[process_index]->workers = kptr->pool_alloc(1);
-	kprc->pool[process_index]->descs = kptr->pool_alloc(2);
-	__non_temporal ptr->calendary[processor_id] = 0;
+	process->workers = kptr->pool_alloc(1);
+	process->descs = kptr->pool_alloc(2);
+	__non_temporal ptr->calendary = 0;
 
-	Kingthread * ktrd; get_kingthread_object(ktrd);
+	Kingthread * ktrd = get_kingthread_object(void);
 	stream_init(kings.THREAD);
-	kprc->pool[process_index]->workers[0] = ktrd->pool_alloc(1);
-	__non_temporal ktrd->calendar[processor_id] = 0
+	process->workers[0] = ktrd->pool_alloc(1);
+	__non_temporal ktrd->calendar = 0;
 
-	Kingdescs * kdescs; get_kingdescriptors_object(kdescs);
+	Kingdescs * kdescs = get_kingdescriptors_object(void);
 	stream_init(kings.DESCRIPTORS);
-	kprc->pool[process_index]->descs[0] = kdescs->pool_alloc(1);
-	kprc->pool[process_index]->descs[1] = kdescs->pool_alloc(1);
-	kprc->pool[process_index]->descs[0]->index = calling_process->descs[0]->index;	//same root context as the parent
-	kprc->pool[process_index]->descs[1]->index = DEVDESC;				//this is just /dev
-	kdescs->calendary[processor_id] = 0;
+	process->descs[0] = kdescs->pool_alloc(1);
+	process->descs[1] = kdescs->pool_alloc(1);
+	process->descs[0]->index = calling_process->descs[0]->index;	//same root context as the parent
+	process->descs[1]->index = DEVDESC;				//this is just /dev
+	kdescs->calendary = 0;
 
 	stream_init(kings.MEMORY);
 	//find the length of the pagetree with the expected memory usage and make the tree
@@ -92,37 +100,36 @@ ustd_t exec(ustd_t findex){
 		level_length *= 512;
 	}
 	ustd_t aligned_pagetree_length;
-	if (pagetree_lenght%4096){ aligned_pagetree_length = pagetre_length/4096+1;}	//losslessly rounding to pagesize
+	if (pagetree_lenght%4096){ aligned_pagetree_length = pagetree_length/4096+1;}	//losslessly rounding to pagesize
 	else{ aligned_pagetree_length = pagetree_length/4096;}
 
-	kprc->pool[process_index]->pagetree = mm->getphys_identity(aligned_pagetree_length,pag.SMALLPAGE);
-	mm->vmtree_lay(kprc->pool[process_index]->pagetree,header->expected_memory_usage);
+	process->pagetree = malloc(aligned_pagetree_length,pag.SMALLPAGE);
+	mm->vmtree_lay(process->pagetree,header->expected_memory_usage);
 
-	//anwyay, writing into the pagetree the executable that was read into memory
-	mm->vmtree_fetch(vfs->descriptions[findex].meta.length,pag.SMALLPAGE);
-	ulong_t * entry = mm->mem_map(executable[0],pagetype,1,cache.WRITEBACK);
+	//mapping the executable file into the pagetree
+	mm->vmtree_fetch(vfs->descriptions[findex].meta.length,source->mapped_pagetype);
+	ulong_t * entry = mm->mem_map(source->shared_contents[0],pagetype,1,cache.WRITEBACK);
 	ustd_t pagetype;
 	entry = mm->vmto_entry(entry,&pagetype);
 	for (ustd_t g = 1; g < vfs->descriptions[findex].meta.length; ++g){
-		mm->mem_map(executable[g],pagetype,1,cache.WRITEBACK);
-		entry[g] |= 1<<63;		//signing the pages as executable
+		entry[g] = mm->memreq_template(pagetype,mode.MAP,cache.WRITEBACK,1);		//signing the pages as executable
 	}
 
 	void * treebackup = mm->vm_ram_table;
-	mm->vm_ram_table = kprc->pool[process_index].pagetree;
+	mm->vm_ram_table = process.pagetree;
 
 	entry = mm->vmtree_fetch(header->stacksize+1,pag.SMALLPAGE);
-	kprc->pool[process_index]->workers[0]->state.stack_ptr = stackp;
+	process->workers[0]->state.stack_ptr = stackp;
 	entry = mm->vmto_entry(entry,&pagetype) + (header->stacksize+1)*8;
 	for (ustd_t h = 0; h < header->stacksize; ++h){				//NOTE HARDENING
 		void * hold = mm->getphys_identity(1,pag.SMALLPAGE);
-		entry* = mm->memreq_template(pag.SMALLPAGE,MAP,WRITEBACK) | hold<<12;
+		entry* = mm->memreq_template(pag.SMALLPAGE,mode.MAP,cache.WRITEBACK,0) | hold<<12;
 		entry -= 8;
 	}
-	entry* = 0;
-
-	__non_temporal mm->calendar[processor_id] = 0;
+	entry* = 0;	//guard page
 
 	mm->vm_ram_table = treebackup;
-	return 0;
+	__non_temporal mm->calendar = 0;
+
+	return process_index;
 }
