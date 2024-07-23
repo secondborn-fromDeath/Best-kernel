3847dd1ec16664916fc68119686b9ac99c225911
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-23 15:06:06 +0000

    Update core.h

--- runtime/sched/core.h
+++ runtime/sched/core.h
@@ -7,11 +7,13 @@
 Class Thread{
 	Process * parent;
 	State * state;			//this includes, at the start, rip,cs,rsp,ss,rflags from the tss and then also all general purpose and vectors
+	ustd_t type;			//module, thread
 	tid_t tid;
 	sig_t sigmask;
 	sig_t sigset;
 	(void (*)(void * data))[MAXSIG];
 	ustd_t stacksize;
+	ustd_t taken;
 };
 
 Class Process{
@@ -20,19 +22,20 @@ Class Process{
 	sig_t sigmask;
 	sig_t sigset;
 	void * pagetree;
+	void * code;
 	tid_t wk_cnt;
 	Thread ** workers;
-	descriptor_t max_desc;	//cant change at process-runtime
-	ustd_t descriptors;	//current
+	descriptor_t max_desc;		//cant change at process-runtime
+	ustd_t descriptors;		//current
 	Descriptor ** descs;
-	ustd_t polls_count;	//if this is above 20 you swap off the pagetree
+	ustd_t polls_count;		//if this is above 20 you swap off the pagetree
+	LDT local_descriptor_table;
 };
 
 Class Processor{
-	privilege_t privilege;	//needed for faults
 	ulong_t current_thread;
-	Thread ** queue;
-	ustd_t queued_threads;
+	ustd_t executed_threads;	//used in rescue_brothers(), as basically "is online"
+	ustd_t online_capable;
 };
 
 
