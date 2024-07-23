24c886bfe30957db28f9751f6abae0a3b14e0acc
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-23 14:51:42 +0000

    Update BLOOD_LIBEL.cpp

--- BLOOD_LIBEL.cpp
+++ BLOOD_LIBEL.cpp
@@ -1,19 +1,21 @@
 /*
-This one evaluates all processes and gives them a BLOOD_score which is made up of their memory usage, owner id and programtype
-
-the algorithm is: memory usage in units of 2mb pages, owner id is multiplied by 50, programtype is multiplied by 100, addition
-Whoever has the highest score gets BOOTED
+The algorithm is: "if not programtype==SYSTEM" memory usage in units of 2mb pages * the number of threads
+Whoever has the highest score gets SACRIFICED
 */
 void BLOOD_LIBEL(void){
-	ProcessesKing * prcking; get_processking_object(prcking);
-	ustd_t sacrifice_index = 0;
-	for (ustd_t i = 0; i < prcking->length; ++i){
-		Process * newsub = prcking->pool[i];
-		Process * champion = prcking->pool[sacrifice_index];
-
-		if !(newsub->memory_usage/512+owner_id*50+program_type*100 < champion->memory_usage/512+champion->owner_id*50+champion->program_type*100){
-			sacrifice_index = i;
+	ProcessesKing * prcking = get_processking_object(void);
+	ustd_t i = 0;
+	while (prcking->pool[i]->program_type == program_types::SYSTEM);
+	Process * champion = &prcking->pool[i];
+	if (i > prcking->length){ shutdown(void);}
+	for (1; i < prcking->length; ++i){
+		Process * contender = &prcking->pool[i];
+		if (contender->program_type != program_types::SYSTEM)&&(prcking->ckarray[i]){
+			contender->sacrival = contender->memory_usage/512*contender*wk_count;
+			if (contender->sacrival > champion->sacrival){
+				champion = contender;
+			}
 		}
 	}
-	prcking->pool[sacrifice_index]->sigset |= signals.SIGKILL;
+	champion->sigset |= signals.SIGKILL;
 }
