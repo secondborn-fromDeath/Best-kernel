087d316f96a6ba7588e5dea4c3541bb27e48ce81
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-23 14:39:47 +0000

    Update uefi.h

--- init/uefi.h
+++ init/uefi.h
@@ -14,7 +14,8 @@ struct efimap_returns{
 	uint64_t * mapsize;
 	void ** map;
 	uint32_t * mapkey;
-	//that is all you need
+	uint32_t descriptor_size;
+	uint32_t descriptor_version;
 };
 struct efimap_descriptor{
 	void * phys_start;
@@ -24,38 +25,58 @@ struct efimap_descriptor{
 	uint32_t descriptor_size;
 	uint32_t pad;
 };
-namespace uefi_memrel{
-	#define MOST_RELIABLE 0x100000
-	#define READONLY 0x200000
-	#define SPEC_PURP 0x400000
-	#define CRYPTO 0x800000
-	#define FIRM_RUNTIME 0x8000000000000000
-	#define ISA_VALID 0x4000000000000000
-	#define ISA_MASK 0x0FFFF00000000000
+void * get_bootservices_table(void * efisystab){		//called in C conv
+	void ** h = efisystab+sizeof(efiheader)+68;
+	return h*;
 }
-void * get_bootservices_table(void ** efisystab){
-	__asm__ volatile(
-	"MOVl	(%%esp,%%eax),%%eax\n\t"
-	"MOVl	68(%%eax),%%eax\n\t"
-	"ret\n\t"
-	::"r"(sizeof(efiheader)):);
-}
-void get_memmap(efimap_returns * returns){
-	uint64_t * bootservices_table = get_bootservices_table(void);
+
+
+void EFIAPI get_memmap(efimap_returns * returns, ustd_t * bootservices_table){
 	auto * uefi_get_memory_map = (bootservices_table+sizeof(efiheader))[32/4];
-	void * memory_map = get_free_identity(1,MIDPAGE);	//so it is big enough that it never fails
-	__asm__ (
-	"MOVq	%%rdx,%%r15\n\t"	//moving pointer into r15
-	"CALL	%%eax\n\t"
-	"MOVq	%%rcx,(%%r15)\n\t"
-	"MOVq	%%rdx,8(%%r15)\n\t"
-	"MOVq	%%r8,16(%%r15)\n\t"
-	::"r"(uefi_get_memory_map),"r"(memory_map),"r"(returns):);
+	returns->mapsize = 512*512*4096;
+	returns->map = get_free_identity(1,MIDPAGE);		//big enough that it never fails
+	(uefi_get_memory_map)(&returns->mapsize,&returns->map,&returns->mapkey,&returns->descriptor_size,&returns->descriptor_version);
+	returns->mapsize /= returns->descriptor_size;
 }
-void exit_boot_services(void * image_handle, uint32_t mapkey){
-	uint64_t * bootservices_table = get_bootservices_table(void);
+void EFIAPI exit_boot_services(void * image_handle, uint32_t mapkey, ustd_t * bootservices_table){
 	auto * uefi_exbootserv = (bootservices_table+sizeof(efiheader))[208/4];
-	__asm__ (
-	"CALL	%%rax\n\t"
-	::"r"(uefi_exbootserv),"r"(imagehandle),"r"(mapkey):);
+	(uefi_exbootserv)(imagehandle,mapkey);
 }
+struct loadfile_returns{
+	char * file;
+	ulong_t length;
+};
+char * EFIAPI loadfile(uchar_t * name, ustd_t * bootservices_table, loadfile_returns * returns){
+	auto * loadfile = (bootservices_table+sizeof(efiheader))[176/4];
+	uint8_t policy = 0;
+	uint32_t sourcesize = 512*512*4096;
+	void * rethandle;
+	ustd_t pages = 1;
+	void * pointer = malloc(1,pag.SMALLPAGE);
+	//NOTE HARDENING
+	while ((loadfile)(policy,imagehandle,name,pointer,sourcesize,&rethandle) != EFI_SUCCESS){
+		++pages;
+		pointer = mrealloc(pointer,pages,pag.SMALLPAGE);
+	}
+	returns->file = pointer;
+	returns->lenght = sourcesize;	//DANGER efi doesnt do a pointer here, kind of weird
+}
+
+namespace uefi_memtypes{	//dont ask to make sense of this because it simply makes no sense and i want no part of it.
+#define STRONG_UNCACHEABLE 0x1
+#define WRITE_COMBINING 0x2
+#define WRITETHROUGH 0x4
+#define WRITEBACK 0x6
+#define WEAK_UNCACHEABLE 0x8
+#define WRITEPROTECT 0x1000
+#define SUPPORTS_READ_PROTECTION 0x2000
+#define SUPPORTS_EXECUTION_PROTECTION 0x4000
+#define PERSISTENT_MEMORY 0x8000
+#define RELIABLE_MEMORY 0x100000
+#define READ_ONLY 0x200000
+#define SPECIAL_PURPOSE 0x400000
+#define SUPPORTS_CRYPTOGRAPHY 0x800000
+#define NEEDS_VIRTUALMAPPING 0x8000000000000000
+#define READ_ISA_MASK 0x0FFFFFFFFFFFFFFF			//i simply ignore this because i am only doing x64 right now and the above are more than enough
+};
+namespace uefi_memtypes_ISA_mask;
