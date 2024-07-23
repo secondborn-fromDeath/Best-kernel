337faaa0005acdf3f39a1c4424b5918429eb8e57
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-23 14:32:34 +0000

    Update UefiMain.cpp

--- init/UefiMain.cpp
+++ init/UefiMain.cpp
@@ -1,26 +1,30 @@
 void cli(void){__asm__("CLI\n\t");}
 void sti(void){__asm__("STI\n\t");}
 
-ustd_t UefiMain(void * image_handle, void * efisystab){
+void processor_init(void){
 	cli(void);
+	setup_MSRS(void);
+	ensure_interrupts_mode(void);
+}
+
+ustd_t UefiMain(void * image_handle, void * efisystab){
+	processor_init(void);
 	enable_A20(void);
-	setup_MSRS(void);			//TODO
-	enable_apic(void);			//TODO
 	efimap_returns data;
 	void * boottab = get_bootservices_table(efisystab);
 	get_uefi_memmap(image_handle,efisystab,&data,boottab);
-	setup_kernel(&data);
-	POST_check(void);			//TODO acpi shutdown
+	char * initsys = setup_kernel(&data);
+	POST_check(void);
 	setup_gdt(void);
 	setup_idt(void);
-	enumerate_devices(void);		//must get done first
-	setup_drivers(void);
-	enumerate_devices(void);		//trust me
-	assign_interrupt_vectors(void);		//TODO
+	enact_IO_context(void);
+	assign_interrupt_vectors(void);
 	sti(void);
 	exit_bootservices(data->mapkey*,boottab);
-	mount(root_disk,root_partition_pos);
-	initialize_brothers(void);		//TODO
-	bystring_exec(init_system);		//TODO wrap
+	initialize_brothers(void);
+	Virtual_fs * vfs = get_vfs_object(void);
+	ustd_t root = vfs->mount(root_disk,root_partition_pos);
+	ustd_t init_index = vfs->open(initsys,root);
+	if (vfs->descriptions[init_index]->type == filetypes.DIRECTORY){ shutdown(void);}		//meaning stand was returned from recurse_directories, should probably log the error... somehow, though
 	routine(void);
 }
