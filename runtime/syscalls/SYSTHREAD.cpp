cdb06ed479fbc5ae7fe679c6afa086518ae1e2e4
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-23 15:03:16 +0000

    Update SYSTHREAD.cpp
    added make_stack to save code between this and exec

--- runtime/syscalls/SYSTHREAD.cpp
+++ runtime/syscalls/SYSTHREAD.cpp
@@ -1,4 +1,21 @@
-//on the tin it says that you are cloning everything (at least until we have flags, TODO) from the calling thread...
+//takes mm with the process' tree already set by the way
+void make_stack(Kingmem * mm, Thread * thread, ){
+	Exec_header * header = thread->father->code;
+
+	void * entry = mm->vmtree_fetch(header->stacksize+1,pag.SMALLPAGE);
+	thread->state.stack_ptr = (entry*)<<5>>18<<1;
+	ustd_t pagetype;
+	entry = mm->vmto_entry(entry,&pagetype) + (header->stacksize+1)*8;
+	for (ustd_t h = 0; h < header->stacksize; ++h){				//NOTE HARDENING
+	    void * hold = mm->getphys_identity(1,pag.SMALLPAGE);
+	    entry* = mm->memreq_template(pag.SMALLPAGE,MAP,WRITEBACK) | hold<<12;
+	    entry -= 8;
+	}
+	entry* = 0;	//guard page
+}
+
+//on the tin it says that you are cloning everyo
+thing (at least until we have flags, TODO) from the calling thread...
 void thread(void){
 	Kingthread * ktrd = get_kingthread_object(void);
 	ktrd->stream_init(void);
@@ -16,16 +33,25 @@ void thread(void){
 	memcpy(newfag.desc_indexes,calling_thread->desc_indexes,calling_thread->desc_count);
 
 	//making a stack for the new thread
-	Process * process = calling_thread->parent;
+	mm->stream_init(void);
 	void * treebackup = mm->vm_ram_table;
 	mm->vm_ram_table = process->pagetree;
-	void * entry = mm->vmtree_fetch(header->stacksize+1,pag.SMALLPAGE);
-	newfag->state.stack_ptr = (entry*)<<5>>18<<1;
-	entry = mm->vmto_entry(entry,&pagetype) + (header->stacksize+1)*8;
-	for (ustd_t h = 0; h < header->stacksize; ++h){				//NOTE HARDENING
-	    void * hold = mm->getphys_identity(1,pag.SMALLPAGE);
-	    entry* = mm->memreq_template(pag.SMALLPAGE,MAP,WRITEBACK) | hold<<12;
-	    entry -= 8;
-	}
-	entry* = 0;	//guard page
+	make_stack(mm,newfag);
+	mm->vm_ram_table = treebackup;
+	__non_temporal mm->calendar = 0;
+
+	//assigning the heap and thread local storage segments
+	newfag->state.fs = 4;
+	newfag->state.gs = process->local_descriptor_table->segment_alloc(1);
 }
+
+
+
+
+
+
+
+
+
+
+
