9156e8c9b5f429171ad1d3bbf62a3d8353879999
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-23 15:01:59 +0000

    Update time.h
    calling convention stuff

--- runtime/syscalls/time.h
+++ runtime/syscalls/time.h
@@ -1,50 +1,34 @@
 struct rtc_time{
 	struct timespec{
-		uint16_t year;
-		uint8_t month;
-		uint8_t day;
-		uint8_t hour;
-		uint8_t minute;
-		uint8_t second;
-		uint8_t pad1;
-		uint32_t nanosecond;
+	uint16_t year;
+	uint8_t month;
+	uint8_t day;
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
+	uint8_t pad1;
+	uint32_t nanosecond;
 	}; sec;
 	signed int16_t timezone;	//-1440 through +1440, 2047 indicates an unknown timezone
 	uint8_t daylight;
 	uint8_t pad2;
 };
 
-ulong_t gettime_nanos(void){		//from the start of the day
-	Kontrol * ctrl; get_kontrol_object(ctrl);
-	struct rtc_time * rtcret = (ctrl->efifuncs.getrtc)(void);
+ulong_t EFIAPI gettime_nanos(void){		//from the start of the day
+	Kontrol * ctrl = get_kontrol_object(void);
+	struct rtc_time rtcret;
+	(ctrl->efifuncs.getrtc)(&rtcret);
 	return rtcret->hour*3600*1000000000 + rtcret->minute*60*10000000000 + rtcret->second*1000000000 + rtcret->nanosecond;
 }
 
-void get_timespec(struct rtc_time * output){
-	Kontrol * ctrl; get_kontrol_object(ctrl);
-	struct rtc_time * rtcret = (ctrl->efifuncs.getrtc)(void);
-	Kingmem * mm; get_kingmem_obejct(mm);
-	Process * process = get_process_object(void);
-	void * backup = mm->vm_ram_table;
-	mm->vm_ram_table = process->pagetree;
-	void * identity = mm->vmto_phys(output);
-	memcpy(identity,rtcret,sizeof(struct rtc_time));
-	mm->vm_ram_table = backup;
+void EFIAPI get_timespec(struct rtc_time * output){
+	Kontrol * ctrl = get_kontrol_object(void);
+	(ctrl->efifuncs.getrtc)(output);
 }
-
-ustd_t set_timespec(struct rtc_time * rtcinsert){	//root things
-	Process * process = get_process_object();
-	if (process->owner_id != users.ROOT){ return 1;}
-	Kingmem * mm; get_kingmem_object(mm);
-	void *backup = mm->vm_ram_table;
-	mm->vm_ram_table = process->pagetree;
-	void ins = vmto_phys(rtcinsert);
-	Kontrol * ctrl; get_kontrol_object(ctrl);
-	ustd_t a;
-	__asm__(
-	"call %%rcx\n\t"				//NOTE bios porting
-	::"r"(ins),"r"(ctrl->efifuncs.setrtc):
-	);
+ustd_t EFIAPI set_timespec(struct rtc_time * rtcinsert){	//root things
+	if (get_process_object(void)->owner_id != users.ROOT){ return 1;}
+	Kontrol * ctrl = get_kontrol_object(void);
+	(ctrl->efifuncs.setrc)(rtcinsert);
 	return 0;
 }
 
@@ -59,30 +43,13 @@ struct KERNwakeup_time{
 	bool pending;
 };
 
-void get_wakeup(struct KERNwakeup_time * output){
-	Kontrol * ctrl; get_kontrol_object(ctrl);
-	struct EFIwakeup_time * wkup_ret = (ctrl->efifuncs.getrtc)(void);
-	Kingmem * mm; get_kingmem_obejct(mm);
-	Process * process = get_process_object(void);
-	void * backup = mm->vm_ram_table;
-	mm->vm_ram_table = process->pagetree;
-	struct KERNwakeup_time * identity = mm->vmto_phys(output);
-	memcpy(&identity->time,wkup_ret,sizeof(struct rtc_time));
-	identity->enabled = wkup_ret->enabled*;
-	identity->pending = wkup_ret->pending*;
-	mm->vm_ram_table = backup;
+void EFIAPI get_wakeup(struct KERNwakeup_time * output){
+	Kontrol * ctrl = get_kontrol_object(void);
+	struct EFIwakeup_time wkup_ret;
+	(ctrl->efifuncs.getrtc)(wkup_ret);
 }
 
-void set_wakeup(struct rtc_time * wkup_insert){
-	Kontrol * ctrl; get_kontrol_object(ctrl);
-	Kingmem * mm; get_kingmem_obejct(mm);
-	Process * process = get_process_object(void);
-	void * backup = mm->vm_ram_table;
-	mm->vm_ram_table = process->pagetree;
-	struct rtc_time * identity = mm->vmto_phys(wkup_insert);
-	bool a = true;
-	__asm__(
-	"call %%rdx\n\t"
-	::"r"(a),"r"(identity),"r"(ctrl->efifuncs.set_wakeup):
-	);
+void EFIAPI set_wakeup(struct rtc_time * wkup_insert){
+	Kontrol * ctrl = get_kontrol_object(void);
+	(ctrl->efifuncs.set_wakeup)(1,identity);
 }
