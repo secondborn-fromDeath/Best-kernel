fe55150e2ee545fb5ccf84022838840a46e76c29
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-27 09:38:11 +0000

    Update time.h

--- runtime/syscalls/time.h
+++ runtime/syscalls/time.h
@@ -13,43 +13,28 @@ struct rtc_time{
 	uint8_t daylight;
 	uint8_t pad2;
 };
-
 ulong_t gettime_nanos(void){		//from the start of the day
 	Kontrol * ctrl = get_kontrol_object(void);
 	struct rtc_time rtcret;
-	uefi_wrapper(ctrl->efifuncs.getrtc,&rtcret,0,0,0,0,0);
+	uefi_wrapper(ctrl->efifuncs->getrtc,&&rtcret,1);	//NOTE lol
 	return rtcret->hour*3600*1000000000 + rtcret->minute*60*10000000000 + rtcret->second*1000000000 + rtcret->nanosecond;
 }
-
 void get_timespec(struct rtc_time * output){
 	Kontrol * ctrl = get_kontrol_object(void);
-	uefi_wrapper(ctrl->efifuncs.getrtc,output,0,0,0,0,0);
+	uefi_wrapper(ctrl->efifuncs->getrtc,&output,1);
 }
 ustd_t set_timespec(struct rtc_time * rtcinsert){	//root things
 	if (get_process_object(void)->owner_id != users.ROOT){ return 1;}
 	Kontrol * ctrl = get_kontrol_object(void);
-	uefi_wrapper(ctrl->efifuncs.setrc,rtcinsert,0,0,0,0,0);
+	uefi_wrapper(ctrl->efifuncs->setrc,&rtcinsert,1);
 	return 0;
 }
-
-struct EFIwakeup_time{
-	bool * enabled;
-	bool * pending;
-	struct rtc_time * wkup_spec;
-};
-struct KERNwakeup_time{
-	struct rtc_time time;
-	bool enabled;
-	bool pending;
-};
-
 void get_wakeup(struct KERNwakeup_time * output){
 	Kontrol * ctrl = get_kontrol_object(void);
 	struct EFIwakeup_time wkup_ret;
-	uefi_wrapper(ctrl->efifuncs.getrtc,wkup_ret,0,0,0,0,0);
+	uefi_wrapper(ctrl->efifuncs->getrtc,&&wkup_ret,1);
 }
-
 void set_wakeup(struct rtc_time * wkup_insert){
 	Kontrol * ctrl = get_kontrol_object(void);
-	uefi_wrapper(ctrl->efifuncs.set_wakeup,1,identity,0,0,0,0);
+	uefi_wrapper(ctrl->efifuncs->set_wakeup,1,&wkup_insert,2);
 }
