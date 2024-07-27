0219dd0c779683215ba44dd20902693311d86da6
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-27 09:37:27 +0000

    Update uefi.h

--- init/uefi.h
+++ init/uefi.h
@@ -25,94 +25,144 @@ struct efimap_descriptor{
 	uint32_t descriptor_size;
 	uint32_t pad;
 };
+struct loadfile_args{
+	char * filename;
+	void * file;
+	uint64_t length;
+};
+#define UEFI_LOADED_IMAGE_PROTOCOL 0x5B1B31A1956211d283F00A0C969723B
+volatile struct image_loaded{	//page 248, this is peak confusion tbh, osdev are larpers LMAO
+	uint32_t revision;
+	void * efisystab;
+	void * handle;
+	void * block_device;
+	void * reserved;
+	uint32_t loadoptions_size;
+	void * loadoptions;
+	void * image_base;
+	uint64_t image_size;
+	uint32_t imagecode_type;	//these are "ENUMS" which is fucking retarded, efi writers are so stupid it is embarrassing...
+	uint32_t imagedata_type;
+	auto * unload_function;
+};
+#define EFI_GRAPHICS_OUTPUT_PROTOCOL 9042a9de23dc4a3896fb7aded080516a
+struct graphics_protocol{
+	auto * query_mode;
+	auto * set_mode;
+	auto * bit;
+	graphics_info * info;
+};
+struct graphics_info{
+	uint32_t maxnode;
+	uint32_t mode;
+	void * something_i_dont_care_about;
+	uint32_t sizeof_above;
+	void * physical_framebuffer;
+	uint32_t framebuffer_size;
+};
+
+
 void * get_bootservices_table(void * efisystab){
 	void ** h = efisystab+sizeof(efiheader)+68;
 	return h*;
 }
-
-volatile void uefi_wrapper(auto * function, auto one,auto two,auto three,auto four, void * additional, ustd_t adds_num){
-	/*
-	Pushing extra arguments, loading the first 4 argument registers, calling with 16byte alignment, popping things off the stack
-	*/
-	__asm__ volatile(
-	"PUSHq	%%r8\n\t"
-	"PUSHq	%%r9\n\t"
-	"PUSHq	%%rbp\n\t"							//frame
-	"MOVq	%%rsp,%%rbp\n\t"
-	"MOVq	40(%%rbp),%%rax\n\t"						//pointer
-	"MOVq	48(%%rbp),%%rcx\n\t"						//argsnum
-	"uefiwrap_addargs:"
-	"TESTq	%%rcx,%%rcx\n\t"
-	"JZ	uefiwrap_call\n\t"
-	"MOVq	(%%rax,%%rcx,8),%%rdx\n\t"
-	"PUSHq	%%rdx\n\t"
-	"DECq	%%rcx\n\t"
-	"JMP	uefi_addargs\n\t"
-	"uefiwrap_call:\n\t"
-	"MOVq	(%%rbp),%%rax\n\t"
-	"MOVq	8(%%rbp),%%rcx\n\t"						//callconv first 4 arguments...
-	"MOVq	16(%%rbp),%%rdx\n\t"
-	"MOVq	24(%%rbp),%%r8\n\t"
-	"MOVq	32(%%rbp),%%r9\n\t"
-	"CALL	%%rax\n\t"
-	"MOVq	48(%%rbp),%%rcx\n\t"						//cleaning the stack
-	"uefiwrap_popping:\n\t"
-	"TESTq	%%rcx,%%rcx\n\t"
-	"JZ	uefiwrap_return\n\t"
-	"POPq	%%rax\n\t"
-	"DECq	%%rcx\n\t"
-	"JMP	uefiwrap_popping\n\t"
-	"uefiwrap_return:\n\t"
-	"POPq	%%rbp\n\t"							//restoring caller state
-	"POPq	%%r9\n\t"
-	"POPq	%%r8\n\t"
-	"RET\n\t"
-	);
+/*
+This is a function that allows the C calling convention to interface with the UEFI callign convention
+Any argument is passed as a qword*/
+volatile void uefi_wrapper(auto * function, uint64_t * args, ustd_t num){
+__asm__ volatile(
+"PUSHq	%%r8\n\t"				//pushing the incongruent calling convention...
+"PUSHq	%%r9\n\t"
+"PUSHq	%%r10\n\t"
+"PUSHq	%%r11\n\t"
+"PUSHq	%%rsi\n\t"				//string instructions
+"PUSHq	%%rdi\n\t"
+"PUSHq	%%rbx\n\t"				//cleaning the stack
+"MOVq	%%rdx,%%rbx\n\t"
+"MOVq	%%rcx,%%rsi\n\t"			//I HATE CLOBBERS! LET ME SPECIFY THE REGISTER MANUALLY INSTEAD OF DOING THIS ENUM NONSENSE
+"MOVq	%%rdx,%%r10\n\t"
+"TESTq	%%r10,%%r10\n\t"
+"JZ	uefiwrapper_call\n\t"
+"MOVq	56(%%rsi),%%rcx\n\t"
+"TESTq	%%r10,%%r10\n\t"
+"JZ	uefiwrapper_call\n\t"
+"MOVq	64(%%rsi),%%rdx\n\t"
+"TESTq	%%r10,%%r10\n\t"
+"JZ	uefiwrapper_call\n\t"
+"MOVq	72(%%rsi),%%r8\n\t"
+"TESTq	%%r10,%%r10\n\t"
+"JZ	uefiwrapper_call\n\t"
+"MOVq	80(%%rsi),%%r9\n\t"
+"MOVq	%%rsp,%%rdi\n\t"
+"ADDq	$32,%%rsi\n\t"
+"uefiwrapper_additionals_loop:"
+"TESTq	%%r10,%%r10\n\t"
+"JZ	uefiwrapper_call\n\t"
+"SUBq	$8,%%rsp\n\t"
+"MOVSq	\n\t"					//assuming direction flag is clear...
+"JMP	uefiwrapper_additionals_loop\n\t"
+"uefiwrapper_call CALL %%rax\n\t"
+"MOVq	%%rbx,%%rcx\n\t"			//see "cleaning the stack"
+"STD	\n\t"					//settign the direction flag and copying back the return values because
+"REPnz	MOVsq"					//si and di are protected by uefi callconv
+"LEAq	(%%rsp,%%rbx,8),%%rsp\n\t"
+"POPq	%%rbx\n\t"
+"POPq	%%rdi\n\t"
+"POPq	%%rsi\n\t"
+"POPq	%%r11\n\t"
+"POPq	%%r10\n\t"
+"POPq	%%r9\n\t"
+"POPq	%%r8\n\t"
+::"r"(function),"r"(args),"r"(num):);
 }
 
 
 //in 4096 byte pages
 void * uefi_allocate_pages(ustd_t pages_number, ulong_t * bootservices_table){
 	auto * allocate_pages = (bootservices_table+sizeof(efiheader))[16/8]
-	void * ret;
-	uefi_wrapper(allocate_pages,0,0,pages_number,&ret,0,0);	//allocate_any, reserve
-	return ret;
+	uint64_t array[] = [NULLPTR,NULLPTR,pages_number,NULLPTR,];			//reserve, allocate any
+	uefi_wrapper(allocate_pages,array,4);
+	return array[3];
 }
 void * uefi_map_pages(ustd_t pages_number, void * request, ulong_t * bootservices_table){
 	auto * allocate_pages = (bootservices_table+sizeof(efiheader))[16/8]
-	void * ret;
-	uefi_wrapper(allocate_pages,2,0,pages_number,&ret);	//allocate_address, reserve			DANGER the reserved type
-	return ret;
+	uint64_t array[] = [2,NULLPTR,pages_number,request];			//reserve, allocate any
+	uefi_wrapper(allocate_pages,array,4);
+	return array[3];
 }
 void get_memmap(efimap_returns * returns, ulong_t * bootservices_table){
 	auto * uefi_get_memory_map = (bootservices_table+sizeof(efiheader))[32/8];
 	returns->mapsize = 512*512*4096;
-	returns->map = uefi_allocate_pages(512);	//big enough that it never fails
-	uefi_wrapper(uefi_get_memory_map,&returns->mapsize,&returns->map,&returns->mapkey,&returns->descriptor_size,&returns->descriptor_version,1);
+	returns->map = uefi_allocate_pages(512);				//big enough that it never fails
+	uint64_t array[] = [returns->mapsize,returns->map,NULLPTR, NULLPTR,];	//would corrupt the calling stack otherwise because the last 2 things are 32bit
+	uefi_wrapper(uefi_get_memory_map,array,4);
+	returns->descriptor_size = array[2];
+	returns->descriptor_version = array[3];
 	returns->mapsize /= returns->descriptor_size;
 }
-void exit_boot_services(void * image_handle, uint32_t mapkey, ulong_t * bootservices_table){
+void exit_boot_services(void * image_handle, uint64_t mapkey, ulong_t * bootservices_table){
 	auto * uefi_exbootserv = (bootservices_table+sizeof(efiheader))[208/8];
-	uefi_wrapper(uefi_exbootserv,imagehandle,mapkey,0,0,0,0);
+	uefi_wrapper(uefi_exbootserv,&image_handle);					//DANGER "optimization"
 }
-struct loadfile_returns{
-	char * file;
-	ulong_t length;
-};
-ustd_t loadfile(uchar_t * name, ulong_t * bootservices_table, loadfile_returns * returns){
+image_loaded * loadfile(loadfile_args * args, ulong_t * bootservices_table, void * parent_handle){
 	auto * loadfile = (bootservices_table+sizeof(efiheader))[176/8];
-	uint8_t policy = 0;
-	uint32_t sourcesize = 512*512*4096;
-	void * rethandle;
-	ustd_t pages = 1;
-	void * pointer = uefi_allocate_pages(1);
-	while (uefi_wrapper(loadfile,policy,imagehandle,name,pointer,sourcesize,&rethandle,1) != EFI_SUCCESS){
-		++pages;
-		pointer = mrealloc(pointer,pages,pag.SMALLPAGE,0);
-		if !(pointer){ return (ustd_t)pointer;}
-	}
-	returns->file = pointer;
-	returns->lenght = sourcesize;	//DANGER efi doesnt do a pointer here, kind of weird
+	void * newimage_handle;
+	uint64_t array[] = [NULLPTR,parent_handle,args->name,NULLPTR,NULLPTR,NULLPTR,&newimage_handle,];
+
+	uefi_wrapper(loadfile,array,7);
+
+	char guid[16] = UEFI_LOADED_IMAGE_PROTOCOL;
+	(uefi_locate_protocol(array,guid,1))(newimage_handle);
+	return ((void **)guid)*;
+}
+void uefi_locate_protocol(char * guid, ulong_t * bootservices_table){				//destroys you guid
+	uefi_wrapper(bootservices_table[280/8],guid,1);
+}
+void boot_get_framebuffer(auto * boottab, Kontrol * ctrl){
+	char pass[16] = UEFI_GRAPHICS_OUTPUT_PROTOCOL;
+	uefi_locate_protocol(pass,boottab);
+	ctrl->framebuffer = ((graphics_info*)pass)->physical_framebuffer;
+	ctrl->framebuffer_size = ((graphics_info*)pass)->framebuffer_size;
 }
 
 namespace uefi_memtypes{	//dont ask to make sense of this because it simply makes no sense and i want no part of it.
