b54470dcdcdcb4ff59ae347b4a892b3db847bfe3
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-28 00:24:39 +0000

    Update SYSTHREAD.cpp
    put the stack at the top of the address space

--- runtime/syscalls/SYSTHREAD.cpp
+++ runtime/syscalls/SYSTHREAD.cpp
@@ -1,56 +1,40 @@
 //takes mm with the process' tree already set by the way
-void make_stack(Kingmem * mm, Thread * thread, ){
+void make_stack(Kingmem * mm, Thread * thread){
 	Exec_header * header = thread->father->code;
+	Process * process = thread->parent;
 
-	void * entry = mm->vmtree_fetch(header->stacksize+1,pag.SMALLPAGE);
-	thread->state.stack_ptr = (entry*)<<5>>18<<1;
-	ustd_t pagetype;
-	entry = mm->vmto_entry(entry,&pagetype) + (header->stacksize+1)*8;
+	std(void);
+	void * entry = stringz(process->pagetree);	//getting the highest entry into the pagetree
+	cld(void);
+
+	thread->state.stack_pointer = entryto_vm(entry);
 	for (ustd_t h = 0; h < header->stacksize; ++h){				//NOTE HARDENING
-	    void * hold = mm->getphys_identity(1,pag.SMALLPAGE);
-	    entry* = mm->memreq_template(pag.SMALLPAGE,MAP,WRITEBACK) | hold<<12;
-	    entry -= 8;
+		entry* = (malloc(1,pag.SMALLPAGE)<<12) | memreq_template(pag.SMALLPAGE,mode.RESERVE,cache.WRITEBACK,0);	//non executable stack
+		entry -= 8;
 	}
 	entry* = 0;	//guard page
 }
 
-//on the tin it says that you are cloning everyo
-thing (at least until we have flags, TODO) from the calling thread...
+//on the tin it says that you are cloning everything (at least until we have flags, TODO) from the calling thread...
 void thread(void){
 	Kingthread * ktrd = get_kingthread_object(void);
-	ktrd->stream_init(void);
 	Thread * newfag = ktrd->pool_alloc(1);
-	__non_temporal ktrd->calendar = 0;
 
 	Thread * calling_thread = get_thread_object(void);
 	memcpy(newfag,calling_thread,sizeof(Thread));
 	newfag->taken = 0;					//newfriend is not being taken care of by anybody unlike oldfren
 
 	Kingptr * kptr = get_pointer_object(void);
-	kptr->stream_init(void);
 	newfag->desc_indexes = kptr->pool_alloc(calling_thread->desc_count);
-	__non_temporal kptr->calendar = 0;
 	memcpy(newfag.desc_indexes,calling_thread->desc_indexes,calling_thread->desc_count);
 
 	//making a stack for the new thread
-	mm->stream_init(void);
-	void * treebackup = mm->vm_ram_table;
-	mm->vm_ram_table = process->pagetree;
 	make_stack(mm,newfag);
-	mm->vm_ram_table = treebackup;
-	__non_temporal mm->calendar = 0;
 
 	//assigning the heap and thread local storage segments
 	newfag->state.fs = 4;
 	newfag->state.gs = process->local_descriptor_table->segment_alloc(1);
 }
-
-
-
-
-
-
-
 
 
 
