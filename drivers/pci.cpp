84318b5a19d530c18a5a04ff23d40af9ad756978
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-28 00:28:03 +0000

    Update pci.cpp
    survived reenaction of the IO context i think?

--- drivers/pci.cpp
+++ drivers/pci.cpp
@@ -6,72 +6,59 @@ What this guy needs to do:
 */
 
 enum pcitypes{ PCI_TO_PCI_DEVICE,PCI_TO_PCI_BRIDGE,};
-Class PciKing : King{
+class PciKing : King{
 	/*
 	Runtime interface for device drivers
 	*/
 	void write(usdt_t dev, ustd_t function, ustd_t * source, ulong_t * offsets, ulong_t count){
 		Virtual_fs * vfs = get_vfs_object(void);
 		Processor * processor = get_processor_object(void);
-		this.stream_init(void);
+		this->stream_init(void);
 
 		ustd_t proto_mask = 1<<31 | vfs->descriptions[dev]->bus<<16 | vfs->descriptions[dev]->device<<11 | function<<8;
 		for (ustd_t g = 0; g < count; ++g){
 			ustd_t mask = proto_mask | offsets[g]>>2<<2;	//bit 1 is 32/64bit so we treat it as reserved since i dont support it
-			__asm__(
-			"OUT %%eax,$0xcf8\n\t"
-			"movl %1,%%eax\n\t"
-			"OUT %%eax,$0xcfc\n\t"
-			::"r"(mask),"m"(source[g]):
-			);
-			__attribute__((optimize("O0"))) for (ustd_t h = processor->frequency/this.frequency*dev->devsel_timing; h; --h);	//either 3 or 4
+			outd(mask,0xcf8);
+			outd(source[g],0xcfc);
+			__attribute__((optimize("O0"))) for (ustd_t h = processor->frequency/this->frequency*dev->devsel_timing; h; --h);	//either 3 or 4
+			}
 		}
-		__non_temporal this.calendar = 0;
+		__non_temporal this->calendar = 0;
 	}
 	void read(ustd_t dev, ustd_t * destination, ulong_t * offsets, ulong_t count){
 		Virtual_fs * vfs = get_vfs_object(void);
 		Processor * processor = get_processor_object(void);
-		this.stream_init(void);
+		this->stream_init(void);
 
 		ustd_t proto_mask = 1<<31 | vfs->descriptions[dev]->bus<<16 | vfs->descriptions[dev]->device<<11 | function<<8;
 		for (ustd_t g = 0; g < count; ++g){
 			ustd_t mask = proto_mask | offsets[g]>>2<<2;
-			__asm__(
-			"OUT %%eax,$0xcf8\n\t"
-			"IN $0xcfc,%%eax\n\t"
-			"movl %%eax,%1\n\t"
-			::"r"(mask),"m"(destination[g]):
-			);
+			outd(mask,0xcf8);
+			destination[g] = ind(0xcfc);
+
 			//here bus frequency is either 33Mhz or 66Mhz, devsel_timing is either 3, 4 or assumed 16
-			__attribute__((optimize("O0"))) for (ustd_t h = processor->frequency/this.frequency*dev->devsel_timing; h; --h);
+			__attribute__((optimize("O0"))) for (ustd_t h = processor->frequency/this->frequency*dev->devsel_timing; h; --h);
+			}
 		}
-		__non_temporal this.calendar = 0;
+		__non_temporal this->calendar = 0;
 	}
 
 	/*
 	Above but for the pci driver itself
 	*/
 	void boot_write(Processor * processor, Device * dev, ustd_t reg, ustd_t contents){
-		__attribute__((optimize("O0"))){ for (ustd_t h = processor->frequency/this.frequency*dev->devsel_timing; h; --h);
+		__attribute__((optimize("O0"))){ for (ustd_t h = processor->frequency/this->frequency*dev->devsel_timing; h; --h);
 		}
 		ustd_t mask = 1<<31 | vfs->descriptions[dev]->bus<<16 | vfs->descriptions[dev]->device<<11 | reg;
-		__asm__(
-		"OUT %%eax,$0xcf8\n\t"
-		"mov %%ecx,%%eax\n\t"
-		"IN %%eax,$0cfc\n\t"
-		::"r"(mask),"r"(contents):
-		);
+		outd(mask,0xcf8);
+		outd(contents,0xcfc);
 	}
 	ustd_t boot_read(Processor * processor, Device * dev, ustd_t reg){
-		__attribute__((optimize("O0"))){ for (ustd_t h = processor->frequency/this.frequency*dev->devsel_timing; h; --h);
+		__attribute__((optimize("O0"))){ for (ustd_t h = processor->frequency/this->frequency*dev->devsel_timing; h; --h);
 		}
 		ustd_t mask = 1<<31 | vfs->descriptions[dev]->bus<<16 | vfs->descriptions[dev]->device<<11 | reg;
-		__asm__(
-		"OUT %%eax,$0xcf8\n\t"
-		"IN $0xcfc,%%eax\n\t"
-		::"r"(mask):
-		);
-		return mask;
+		outd(mask,0xcf8);
+		return ind(0xcfc);
 	}
 	/*
 	The boot interface
@@ -79,43 +66,61 @@ Class PciKing : King{
 
 	bus,dev,fun need to be passed as 0 (*==0 lol) initially
 	*/
-	ustd_t enumerate_devices(Processor * processor, Virtual_fs * vfs, DriversGod * drivgod, ustd_t bus_number, ustd_t * device_number, ustd_t function){
-		if (device_number* == 32){ return 0;}	//out
-		Device * newdev = vfs->pool_alloc(1);
-		newdev->bus = bus_number;
-		newdev->device = device_number*;
-		newdev->function = function;
-		newdev->devsel_timing = get_devsel_timing(processor,newdev);	//see boot_read/write
-		newdev->identification = get_idinfo(processor,newdev);
-		if !(newdev->identification == -1){				//first one that can fail AND be checkable
-			++this.enumerated_devices;
-			newdev->geninfo = get_geninfo(processor,newdev);
-			((ushort_t*)&newdev->irline)[0] = get_irline_and_pin(processor,bus_number,device_number*);
-			ustd_t multifunction_boo;
-			ustd_t headertype = get_headertype(processor,newdev,&multifunction_boo);
-			set_device_ranges(processor,newdev,headertype);
-			newdev->expansion_rom = get_expansionrom(processor,newdev);
-			newdev->multifunction_boo = get_multifunction_boolean(processor,newdev);
-
-			newdev -> driver = NULL;	//NOTE ioctl needs to check for this
-			for (ustd_t g = 0; g < drivgod->length; ++g){
-				if !(vfs->descriptions[drivgod->drivers[g]]->driv->code->check_classcode(newdev->geninfo)){	//ugly but its too long
-				if !(vfs->descriptions[drivgod->drivers[g]]->driv->code->attach_model(newdev))){
-					break;}}
-			}
-			if (headertype == PCI_TO_PCI_DEVICE){
-				device_enum(processor,vfs,get_secondarybus_number(processor,newdev));
-			}
-			if ((multifunction_boo) && (!function)){
-				for (ustd_t g = 1; g < 4; ++g){
-					device_enum(processor,vfs,drivgod,bus_number,device_number,g);
+	ustd_t enumerate_devices(Processor * processor, Virtual_fs * vfs, Directory * devices, DriversGod * drivgod, ustd_t bus_number, ustd_t * device_number, ustd_t function, ustd_t re){
+		while !(device_number* == 32){
+			Device * newdev = vfs->pool_alloc(1);
+			devices->children->pool_alloc(1)* = newdev;	//legal sepple? DANGER
+			newdev->bus = bus_number;			//NOTE OPTIMIZATION, this is unacceptable. It should be behind the conditional and then the boot-whatever should take the raw arguments
+			newdev->device = device_number*;
+			newdev->function = function;
+			newdev->devsel_timing = get_devsel_timing(processor,newdev);	//see boot_read/write
+			newdev->identification = get_idinfo(processor,newdev);
+			for (ustd_t i = 0; i < devices->length; ++i){		//attempt to see wether from the back up the device is still there (through teh model id)
+				if (devices->pool[i]->identification == newdev->identification){
+					vfs->pool_free(newdev,1);
+					goto devenum_bail;
 				}
 			}
+			if !(newdev->identification == -1){				//first one that can fail AND be checkable
+				++this->enumerated_devices;
+				newdev->geninfo = get_geninfo(processor,newdev);
+				((ushort_t*)&newdev->irline)[0] = get_irline_and_pin(processor,bus_number,device_number*);
+				ustd_t multifunction_boo;
+				ustd_t headertype = get_headertype(processor,newdev,&multifunction_boo);
+				set_device_ranges(processor,newdev,headertype);
+				newdev->expansion_rom = get_expansionrom(processor,newdev);
+				newdev->multifunction_boo = get_multifunction_boolean(processor,newdev);
 
+				newdev -> driver = NULL;
+				for (ustd_t g = 0; g < drivgod->length; ++g){
+					fiDriv * fidriv = drivgod->pool[g];
+					if !(fidriv->d->code->check_classcode(newdev->geninfo)){	//ugly but its too long
+					if !(fidriv->d->code->attach_model(newdev))){
+						fidriv->initialize_device(newdev);			//this may enumerate the new usb/ahci/whatever bus...
+
+						Thread * thread = get_thread_object(void);
+						set_thread(&fidriv->runtime->children->pool[0]);
+						newdev->thread = thread(void);
+						newdev->thread->application = thread_types::DRIVER;
+						set_thread(thread);
+					}
+					break;}}
+				}
+
+				if (headertype == PCI_TO_PCI_DEVICE){
+					device_enum(processor,vfs,get_secondarybus_number(processor,newdev),0,0,re);	//enumerating the new bus
+				}
+				if ((multifunction_boo) && (!function)){
+					for (ustd_t g = 1; g < 4; ++g){
+						device_enum(processor,vfs,drivgod,bus_number,device_number,g,re);
+					}
+				}
+
+			}
+			else{vfs->pool_free(newdev,1);}
+
+devenum_bail:		++device_number;
 		}
-		else{vfs->pool_free(newdev,1);}
-		++device_number*;
-		return 1;	//keep going
 	}
 		/*
 	A bunch of get/set_XXX info from configuration space -type functions
@@ -123,11 +128,8 @@ Class PciKing : King{
 	*/
 	ustd_t get_devsel_timing(Processor * processor, Device * dev){
 		ustd_t mask = 1<<31 | dev->bus<<16 | dev->device<<11 | 0x4;
-		__asm__ (
-		"OUT %%eax,$0xcf8\n\t"
-		"IN $0xcfc,%%eax\n\t"
-		::"r"(mask):"%rcx"
-		);
+		outd(mask,0xcf8);
+		mask = ind(0xcfc);
 		return mask<<22>>30;	//extracting the 2 bits from status-command
 	}
 	ustd_t get_headertype(Processor * processor, Device * dev, ustd_t * multifunction){
