958c36928d37f522c3947c517c4fa08a3e845ba5
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-28 04:08:47 +0200

    Update SYSTHREAD.cpp

--- runtime/syscalls/SYSTHREAD.cpp
+++ runtime/syscalls/SYSTHREAD.cpp
@@ -1,18 +1,17 @@
 //takes mm with the process' tree already set by the way
-void make_stack(Kingmem * mm, Thread * thread){
+void make_stack(void * pagetree, Thread * thread){
 	Exec_header * header = thread->father->code;
-	Process * process = thread->parent;
 
 	std(void);
-	void * entry = stringz(process->pagetree);	//getting the highest entry into the pagetree
+	void * entry = stringz(pagetree);	//getting the highest entry into the pagetree
 	cld(void);
 
-	thread->state.stack_pointer = entryto_vm(entry);
+	thread->state.stack_pointer = entryto_vm(pagetree,entry);
 	for (ustd_t h = 0; h < header->stacksize; ++h){				//NOTE HARDENING
 		entry* = (malloc(1,pag.SMALLPAGE)<<12) | memreq_template(pag.SMALLPAGE,mode.RESERVE,cache.WRITEBACK,0);	//non executable stack
 		entry -= 8;
 	}
-	entry* = 0;	//guard page
+	entry* = NULLPTR;	//guard page
 }
 
 //on the tin it says that you are cloning everything (at least until we have flags, TODO) from the calling thread...
@@ -29,7 +28,7 @@ void thread(void){
 	memcpy(newfag.desc_indexes,calling_thread->desc_indexes,calling_thread->desc_count);
 
 	//making a stack for the new thread
-	make_stack(mm,newfag);
+	make_stack(calling_thread->parent->pagetree,newfag);
 
 	//assigning the heap and thread local storage segments
 	newfag->state.fs = 4;
