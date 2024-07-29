29d52786af4969bef6ab87c6dd4a26a2d600d3d1
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-29 09:53:23 +0000

    Delete kernlib/malloc.h

--- kernlib/malloc.h
+++
@@ -1,8 +0,0 @@
-//this is to be used by in-kernel functions
-
-void * malloc(ustd_t pages_number, ustd_t pagetype){
-	Kingmem * mm = get_kingmem_object(void);
-	void * ret = mm->get_free_identity(pages_number,pagetype);
-	mm->manipulate_phys(ret,pages_number,pagetype,SET);
-	return ret;
-}
