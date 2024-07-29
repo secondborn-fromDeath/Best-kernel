f0d7d905b0abda29d5e0cde6c7583df14e271673
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-29 09:50:07 +0000

    Update SYSFMAP.cpp

--- runtime/syscalls/SYSFMAP.cpp
+++ runtime/syscalls/SYSFMAP.cpp
@@ -1,14 +1,20 @@
-void * fmap(ustd_t findex, ustd_t amount, ustd_t offset){	//offset is absolute and you are guaranteed that at least the smallpage under it is safe, amount is rounded up to the file's pagetype, determined at mount-time
+//amoutn and offset are absolute because the mapped_pagetype for the file is transparent to the process
+void * fmap(ustd_t findex, ustd_t amount, ustd_t offset){
 	Kingmem * mm = get_kingmem_object(void);
 	Virtual_fs * vfs = get_vfs_object(void);
 	Thread * calling_thread = get_thread_object(void);
 	Process * calling_process = calling_thread->parent;
+	DisksKing * dking = get_disksking_object(void);
 
 	File * file = &vfs->descriptions[findex];
-	ustd_t filemulti = mm->get_multi_from_pagetype(file->mapped_pagetype);
-	ustd_t passlen = amount;
-	tosmallpage(passlen);
-	void * ret = mm->mem_map(process->pagetree,file->shared_contents[offset/filemulti],file->mapped_pagetype,passlen,cache.WRITEBACK) + offset%filemulti;
+	ustd_t filemulti = mm->get_multi_from_pagetype(file->shared_contents->mapped_pagetype);
+	ustd_t g = amount/filemulti;
+	ulong_t * entry = mm->vmtree_fetch(calling_process->pagetree,file->shared_contents->mapped_pagetype,g);
+	entry = vmto_entry(calling_process->pagetree,entry);
+	for (1; g; --g){	//NOTE DOUBLE FAULT HARDENING
+		entry[g] = vfs->load_page(dking,file,(offset/filemulti)+g) | memreq_template(file->shared_contents->mapped_pagetype,mode.MAP,cache.WRITEBACK,0);;
+		++file->shared_contents->ckarray[(offset/multi)+g];
+	}
 
-	return ret;
+	return ret+offset;
 }
