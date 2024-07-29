6e5273c29a564d5ace5242a9ae9234534d53b071
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-29 09:54:49 +0000

    Update report_IO.cpp

--- kernlib/report_IO.cpp
+++ kernlib/report_IO.cpp
@@ -6,8 +6,8 @@ void report_IO(ustd_t findex, ustd_t action){		//takes an index into the vfs, no
 	ProcessesKing * pking = get_processking_object(void);
 	for (ustd_t i = 0; i < pking->length; ++i){
 		Process * process = &pking->pool[i];
-		for (ustd_t k = 0; k < process->wk_count; ++k){
-			Thread * thread = &process->workers[k];
+		for (ustd_t k = 0; k < process->workers->count; ++k){
+			Thread * thread = &process->workers->pool[k];
 			for (ustd_t w = 0; w < thread->pollnum){
 				pollfd * poll = &thread->poll[w];
 				if (poll)&&(process->descs[poll->fdesc]->findex == findex){
