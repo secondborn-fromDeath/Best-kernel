6544e6505d49b172d55e976de684ac5a547cb9de
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-31 16:19:55 +0000

    Update 001_SYSREAD.cpp

--- runtime/syscalls/001_SYSREAD.cpp
+++ runtime/syscalls/001_SYSREAD.cpp
@@ -1,6 +1,12 @@
 volatile void SYSREAD(ustd_t descriptor, void * buf, ulong_t amount, ulong_t offset){
 	Virtual_fs * vfs = get_vfs_object(void);
-	void * truebuf = get_kingmem_object(void)->vmto_phys(buf);
+	Thread * thread = get_thread_object(void);
+	Process * process = thread->parent;
 
-	vfs->read(get_process_object(void)->descs[descriptor]->findex,truebuf,amount,offset);
+	CONDITIONAL_SYSRET(thread,(!(process->descs->count > descriptor)||(process->descs->ckarray[descriptor] == 0)),-1);
+
+	void * truebuf = get_kingmem_object(void)->vmto_phys(process->pagetree,buf);
+	vfs->read(process->descs->pool[descriptor]->findex,truebuf,amount,offset);
+	thread->sys_retval = amount;
+	SYSRET;
 }
