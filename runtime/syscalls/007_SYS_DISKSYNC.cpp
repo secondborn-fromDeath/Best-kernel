d063e8ce8c5dd0ea4ebc5a331b85435a3020d4b4
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-07-31 16:25:27 +0000

    Create 007_SYS_DISKSYNC.cpp

---
+++ runtime/syscalls/007_SYS_DISKSYNC.cpp
@@ -0,0 +1,16 @@
+/*
+This synchronizes the file on disk with the contents of the file in memory*/
+void SYS_DISKSYNC(ustd_t desc){
+	Thread * thread = get_thread_object(void);
+	Process * process = thread->parent;
+	Virtual_fs * vfs = get_vfs_object(void);
+	Disksking * dking = get_disksking_object(void);
+
+	CONDITIONAL_SYSRET(thread,(!(process->descs->count > desc)||(process->descs->ckarray[desc] == 0)),-1);
+
+	File * file = &vfs->descriptions[process->descs->pool[desc]->findex];
+	dking->disk_sync(file);
+
+	thread->sys_retval = 0;
+	SYSRET;
+}
