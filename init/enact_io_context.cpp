bdf621a1dbe4161822931efa6cbd959611266e11
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-02 16:18:31 +0000

    Update enact_io_context.cpp

--- init/enact_io_context.cpp
+++ init/enact_io_context.cpp
@@ -1,16 +1,25 @@
 /*
 Creating the IO context*/
+using enum device_driver_routines;
 void enact_IO_context(ustd_t drivers_number, image_loaded ** driver_pointers, image_loaded * info_pointers){
-	Kingprocess * kprc = get_kingprocess_object(void);
-	Virtual_fs * vfs = get_vfs_object(void);
+	Kingprocess * kprc = get_kingprocess_object(NUH);
+	Virtual_fs * vfs = get_vfs_object(NUH);
+	Kingthread * ktrd = get_threadking_object(NUH);
+	Thread * ori_thread = get_thread_object(NUH);
 
+	Thread * kernel;
+	INIT_KERNEL_THREAD(kernel,tking);
+	kernel->prior = ori_thread;
 	for (ustd_t i = 0; i < drivers_number; ++i){
 		++drivers_number;
 		Storage * fdriv = vfs->ramcontents_to_description(driver_pointers[drivers_number]->image_pointer,driver_pointers[drivers_number]->image_size,get_pagetype_from_number(driver_pointers[drivers_number]->image_size));
 		drivgod->pool_alloc(1)* = fdriv;
-		fdriv->runtime = kprc->pool[exec(fdriv)];
-		fdriv->runtime->code->models = vfs->ramcontents_to_description(driver_pointers[drivers_number]->image_pointer,driver_pointers[drivers_number]->image_size,get_pagetype_from_number(info_pointers[drivers_number]->image_size));
-		memcpy(&fdriv->meta.name,&fdriv->runtime->code->name,sizeof(File.meta.name));
+		fdriv->d->runtime = kprc->pool[exec(fdriv)];
+		fdriv->d->runtime->code->models = vfs->ramcontents_to_description(driver_pointers[drivers_number]->image_pointer,driver_pointers[drivers_number]->image_size,get_pagetype_from_number(info_pointers[drivers_number]->image_size));
+		memcpy(&fdriv->meta.name,&fdriv->d->runtime->code->name,sizeof(File.meta.name));
+		module_ctl(fdriv,GET_MODE_IRHANDLER,NULLPTR);
 	}
-	load_all_devices(void);
+	load_all_devices(NUH);
+	set_thread(kernel->prior);
+	TKILL(kernel);
 }
