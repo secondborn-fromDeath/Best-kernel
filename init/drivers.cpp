30b4824fc186df00f9686dbc17f234b1262d989f
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-02 05:25:03 +0000

    Update drivers.cpp

--- init/drivers.cpp
+++ init/drivers.cpp
@@ -6,7 +6,7 @@ NOTE HARDENING for the filelength
 
 ustd_t get_drivers_number(char * config){
 	ustd_t ret = 0;
-	for (ustd_t k = 0; config[k] != '\n'; ++k){				//DANGER if eval is before increase...
+	for (ustd_t k = 0; config[k] != '\n'; ++k){
 		char * drivername = string+8;
 		ustd_t j;
 		for (j = 0; ((drivername[j] != '\n')&&(drivername[j] != '#'); ++j){
@@ -17,32 +17,37 @@ ustd_t get_drivers_number(char * config){
 	}
 	return ret;
 }
-void load_drivers(void * bootservices_table, void ** driver_vmpointers, image_loaded * drivconf_image){
+void load_drivers(void * bootservices_table, image_loaded ** driver_pointers, image_loaded ** info_pointers, image_loaded * drivconf_image){
 	char * drivconf = (char *)drivconf_image->image_base;
 	char string[64];
 	string[0] = 0x2F64726976657273;	//"/drivers"
 	ustd_t driver_cnt = 0;
-	for (ustd_t k = 0; drivconf[k] != '\n'; ++k){				//DANGER if eval is before increase...
+	for (ustd_t k = 0; drivconf[k] != '\n'; ++k){
 		char * drivername = string+8;
 		ustd_t j;
 		for (j = 0; ((drivername[j] != '\n')&&(drivername[j] != '#'); ++j){
 			drivername[j] = drivconf[k];
 			++k;
 		}
-		drivername[j+1] = 0;
+		++j;
+		drivername[j] = 0;
 		loadfile_args temp;
-		driver_vmpointer[driver_cnt] = uefi_loadfile(string,bootservices_table,&temp);
+		driver_pointers[driver_cnt] = uefi_loadfile(string,bootservices_table,&temp);
 		++driver_cnt;
 
+		//loading the info file
+		ulong_t * info = &drivername[j];
+		info* = 0x2E696E666F;								//DANGER unaligned access on pointer arithmetic...
+		info_pointers[driver_cnt] = uefi_loadfile(string,bootservices_table,&temp);
+
 		if (drivconf[k] != '\n'){ while (drivconf[k] != '\n'){ ++k};}
-		if (k -> drviconf_image->image_size){ shutdown(void);}			//bad configuration file..
+		if (k -> drviconf_image->image_size){ shutdown(void);}				//bad configuration file...
 	}
 }
 //outsourcing uefimain logic because aesthetics
-ustd_t initialize_drivers(void * bootservices_table, void ** driver_vmpointers){
+ustd_t fetch_drivers_basics(void * bootservices_table, image_loaded ** drivconf_image){
 	uint64_t drivconf = 0x2F647269762E6366;	// "/driv.cf"
 	loadimage_args temp;
-	image_loaded * drivconf_image = uefi_loadimage(&drivconf,&temp);		//actually check it lol
-	ustd_t ret = get_drivers_number(temp->pointer);
-	load_drivers(bootservices_table,driver_vmpointers,drivconf_image);
+	drivconf_image* = uefi_loadimage(&drivconf,&temp);
+	return get_drivers_number(temp->pointer);
 }
