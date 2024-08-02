00811204f5eac59da277ac840828fd03e253c416
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-02 05:25:34 +0000

    Update enact_io_context.cpp

--- init/enact_io_context.cpp
+++ init/enact_io_context.cpp
@@ -1,14 +1,16 @@
 /*
-Creating the IO context, devices can be inserted without drivers but drivers cant without devices....*/
-void enact_IO_context(void ** driver_pointers, efimap_returns * data){
-	load_all_devices(void);
-	ustd_t drivnum = 0;
-	for (ustd_t i = data->map_size; i; --i){
-		if (data->map->vm_pointer == driver_pointer[drivnum]){
-			++drivnum;
-			Storage * fdriv = vfs->ramcontents_to_description(driver_pointers[drivnum]->image_pointer,driver_pointers[drivnum]->image_size);
-			drivgod->pool_alloc(1) = fidriv;
-		}
+Creating the IO context*/
+void enact_IO_context(ustd_t drivers_number, image_loaded ** driver_pointers, image_loaded * info_pointers){
+	Kingprocess * kprc = get_kingprocess_object(void);
+	Virtual_fs * vfs = get_vfs_object(void);
+
+	for (ustd_t i = 0; i < drivers_number; ++i){
+		++drivers_number;
+		Storage * fdriv = vfs->ramcontents_to_description(driver_pointers[drivers_number]->image_pointer,driver_pointers[drivers_number]->image_size,get_pagetype_from_number(driver_pointers[drivers_number]->image_size));
+		drivgod->pool_alloc(1)* = fdriv;
+		fdriv->runtime = kprc->pool[exec(fdriv)];
+		fdriv->runtime->code->models = vfs->ramcontents_to_description(driver_pointers[drivers_number]->image_pointer,driver_pointers[drivers_number]->image_size,get_pagetype_from_number(info_pointers[drivers_number]->image_size));
+		memcpy(&fdriv->meta.name,&fdriv->runtime->code->name,sizeof(File.meta.name));
 	}
 	load_all_devices(void);
 }
