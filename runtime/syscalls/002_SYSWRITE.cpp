3a4320efec070144bbb5672f90c1234d446081ba
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-02 05:27:12 +0000

    Update 002_SYSWRITE.cpp

--- runtime/syscalls/002_SYSWRITE.cpp
+++ runtime/syscalls/002_SYSWRITE.cpp
@@ -5,8 +5,11 @@ void SYSWRITE(ustd_t desc, void * buf, ustd_t amount, ustd_t offset){
 
 	CONDITIONAL_SYSRET(thread,(!(process->descs->count > desc)||(process->descs->ckarray[desc] == 0)),-1);
 
-	void * pass = vmto_phys(process->pagetree,buf);
-	vfs->write(process->descs->pool[desc]->findex,truebuf,amount,offset);		//we need to make teh above into a function and harden these check because shit is unacceptable
+	File * file = &vfs->descriptions[process->descs->pool[desc]->findex];
+	CONDITIONAL_SYSRET(thread,((file->meta.mode & permissions::WRITE)&&(process->owner_id != owner_ids::ROOT)));
+
+	void * truebuf = vmto_phys(process->pagetree,buf);
+	vfs->write(findex,truebuf,amount,offset);
 	thread->sys_retval = amount;
 	SYSRET;
 }
