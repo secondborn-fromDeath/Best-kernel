292033e13a63ca95f153b13c0a21f8255797633a
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-02 05:29:31 +0000

    Update 009_SYS_CHMETA.cpp

--- runtime/syscalls/009_SYS_CHMETA.cpp
+++ runtime/syscalls/009_SYS_CHMETA.cpp
@@ -9,11 +9,10 @@ void SYS_CHMETA(ustd_t desc, char * name, ustd_t type, ustd_t mode){
 
 	CONDITIONAL_SYSRET(thread,(!(process->descs->count > desc)||(process->descs->ckarray[desc] == 0)),1);
 
-	auto * truename = mm->vmto_phys(process->pagetree,name);
 	File * file = &vfs->descriptions[process->descs->pool[desc]->findex];
+	CONDITIONAL_SYSRET(thread,((file->meta.mode & permissions::WRITE)&&(process->owner_id != owner_ids::ROOT)),2);
 
-	CONDITIONAL_SYSRET(thread,thread->owner_id < (file->meta.mode<<6>>7),2);
-
+	auto * truename = mm->vmto_phys(process->pagetree,name);
 	memcpy(&file->meta.name,truename,sizeof(File.meta.name));
 	file->meta.type = type;
 	file->meta.mode = mode;
