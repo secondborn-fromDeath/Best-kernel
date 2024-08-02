8673e7a200a78f3a4aa81ad963af0ce56c0faaba
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-02 05:27:53 +0000

    Update 005_SYSRMFILE.cpp

--- runtime/syscalls/005_SYSRMFILE.cpp
+++ runtime/syscalls/005_SYSRMFILE.cpp
@@ -8,8 +8,8 @@ void SYS_RMFILE(ustd_t desc){
 
 	CONDITIONAL_SYSRET(thread,(!(process->descs->length > desc)||(process->descs->ckarray[desc] == 0)),1);
 
-	Storage * file = &vfs->descriptions[process->descs->pool[desc]->findex];
-	CONDITIONAL_SYSRET(thread,(file->mode<<6>>7) > process->owner_id,2);		//permissions
+	File * file = &vfs->descriptions[process->descs->pool[desc]->findex];
+	CONDITIONAL_SYSRET(thread,((file->meta.mode & permissions::WRITE)&&(process->owner_id != owner_ids::ROOT)),2);
 
 	dking->rmfile(file);
 	file->parent->children->pool_free(file->double_link,1);
@@ -18,4 +18,3 @@ void SYS_RMFILE(ustd_t desc){
 	thread->sys_retval = 0;
 	SYSRET;
 }
-
