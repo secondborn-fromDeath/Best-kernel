fe47d827c9ceb312507ca463548f928033f988c8
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-02 05:29:14 +0000

    Update 008_SYSSTAT.cpp

--- runtime/syscalls/008_SYSSTAT.cpp
+++ runtime/syscalls/008_SYSSTAT.cpp
@@ -4,13 +4,17 @@ void SYS_STAT(ustd_t desc, meta * returns){
 	Thread * thread = get_thread_object(void);
 	Process * process = thread->parent;
 	Kingmem * mm = get_kingmem_object(void);
+	Virtual_fs * vfs = get_vfs_object(void);
 
 	CONDITIONAL_SYSRET(thread,(!(process->descs->count > desc)||(process->descs->ckarray[desc] == 0)),1);
+
+	File * file = vfs->descriptions[process->descs->pool[desc]->findex];
+	CONDITIONAL_SYSRET(thread,((file->meta.mode & permissions::WRITE)&&(process->owner_id != owner_ids::ROOT)),2);
 
 	auto * true_ret = mm->vmto_phys(process->pagetree,returns);
 	CONDITIONAL_SYSRET(thread,true_ret == 0,2);
 
-	memcpy(true_ret,&get_vfs_object(void)->descriptions[process->descs->pool[descs]->findex]->meta,sizeof(meta));
+	memcpy(true_ret,&vfs->descriptions[process->descs->pool[descs]->findex]->meta,sizeof(meta));
 	thread->sys_retval = 0;
 	SYSRET;
 }
