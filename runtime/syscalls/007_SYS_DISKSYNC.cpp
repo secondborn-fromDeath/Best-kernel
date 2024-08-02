dc681ed5b402a9aca380996faa17be5b55a631cc
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-02 05:28:54 +0000

    Update 007_SYS_DISKSYNC.cpp

--- runtime/syscalls/007_SYS_DISKSYNC.cpp
+++ runtime/syscalls/007_SYS_DISKSYNC.cpp
@@ -9,6 +9,8 @@ void SYS_DISKSYNC(ustd_t desc){
 	CONDITIONAL_SYSRET(thread,(!(process->descs->count > desc)||(process->descs->ckarray[desc] == 0)),-1);
 
 	File * file = &vfs->descriptions[process->descs->pool[desc]->findex];
+	CONDITIONAL_SYSRET(thread,((file->meta.mode & permissions::WRITE)&&(process->owner_id != owner_ids::ROOT)),-2);
+
 	dking->disk_sync(file);
 
 	thread->sys_retval = 0;
