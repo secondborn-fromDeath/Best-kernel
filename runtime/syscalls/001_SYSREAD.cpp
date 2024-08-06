880b7faac5b06af6481fd9abebb4f481e3552de5
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-06 18:59:13 +0000

    Update 001_SYSREAD.cpp

--- runtime/syscalls/001_SYSREAD.cpp
+++ runtime/syscalls/001_SYSREAD.cpp
@@ -1,3 +1,6 @@
+/*
+This reads from a file in either memory or disk into the specifed buffer, the file pointer may be overriden,
+the file pointer exists for things like the windowing system*/
 void SYSREAD(ustd_t descriptor, void * buf, ulong_t amount, ulong_t offset){
 	Virtual_fs * vfs = get_vfs_object(void);
 	Thread * thread = get_thread_object(void);
@@ -9,7 +12,14 @@ void SYSREAD(ustd_t descriptor, void * buf, ulong_t amount, ulong_t offset){
 	CONDITIONAL_SYSRET(thread,(file->meta.mode & action)&&(process->owner_id != owner_ids::ROOT),errcode);
 
 	void * truebuf = get_kingmem_object(void)->vmto_phys(process->pagetree,buf);
-	vfs->read(file,truebuf,amount,offset);
+	if !(offset){
+		ulong_t * val = &process->descs->pool[descriptor]->file_offset;
+		vfs->read(file,truebuf,amount,val*);
+		val* += amount;
+	}else{
+		vfs->read(file,truebuf,amount,offset);
+	}
+
 	thread->sys_retval = amount;
 	SYSRET;
 }
