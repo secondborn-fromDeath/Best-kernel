b65bb954575dec374f85f65d2a208ed4642adde6
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-06 18:51:28 +0000

    Update UefiMain.cpp
    i read in some manual that asserting a20 outside of real mode is undefined behaviour and this is uefi, took off post-check too, because

--- init/UefiMain.cpp
+++ init/UefiMain.cpp
@@ -1,16 +1,15 @@
-void cli(void){__asm__("CLI\n\t");}
-void sti(void){__asm__("STI\n\t");}
+void cli(void){__asm__ volatile("CLI\n\t");}
+void sti(void){__asm__ volatile("STI\n\t");}
 
 void processor_init(void){
-	cli(void);
-	setup_MSRS(void);
-	ensure_interrupts_mode(void);
+	cli(NUH);
+	setup_MSRS(NUH);
+	ensure_interrupts_mode(NUH);
 }
 
 ustd_t UefiMain(void * image_handle, void * efisystab){
-	processor_init(void);
-	if !(test_ps2_controller(void)){ shutdown(void);}
-	enable_A20(void);							//in uefi this *should* not be needed but whatever
+	processor_init(NUH);
+	if !(test_ps2_controller(NUH)){ shutdown(NUH);}
 	void * boottab = get_bootservices_table(efisystab);
 	image_loaded * config;
 	ustd_t drivers_number = fetch_drivers_basics(boottab,&config);
@@ -19,17 +18,18 @@ ustd_t UefiMain(void * image_handle, void * efisystab){
 	efimap_returns mapdata;
 	get_uefi_memmap(image_handle,efisystab,&mapdata,boottab);
 	char * initsys = setup_kernel(efisystab,boottab,&mapdata);		//from here on out you can use the kernel memory allocation functions
-	POST_check(void);
+	Pci * pci = get_pci_object(NUH);
+	pci->pci_init(NUH);
 	enact_IO_context(driver_pointers,mapdata);
-	setup_gdt(void);
-	setup_idt(void);
+	setup_gdt(NUH);
+	setup_idt(NUH);
 	ustd_t len, init_index;
 	vfs->open(initsys,0,&len,&index);
 	exec(init_index);
 	ustd_t root = vfs->mount(root_disk,root_partition_pos);
 	exit_bootservices(mapdata->mapkey,boottab);
-	assign_interrupt_vectors(void);						//inside here we are initializing gdt vectors by the way
-	sti(void);
-	initialize_brothers(void);
-	routine(void);
+	assign_interrupt_vectors(NUH);						//inside here we are initializing gdt vectors by the way
+	sti(NUH);
+	initialize_brothers(NUH);
+	routine(NUH);
 }
