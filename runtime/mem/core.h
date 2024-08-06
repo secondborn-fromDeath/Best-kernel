4f8065fdf87b02d9fcbf6ba84c6fced9d2ccd6a5
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-06 18:55:00 +0000

    Update core.h
    i must have been drunk when i wrote the other code, anyway because the kernel always needs to be mapped in the userspace process' pagetree i decided to just make all trees 2mb (the max for 4level paging)

--- runtime/mem/core.h
+++ runtime/mem/core.h
@@ -40,12 +40,12 @@ all mutexes are called internally
 		.swap_process()				goes through a process' pagetree and swaps entries out to disk
 
 Bits:
-		9					if set it means the page is taken, for directories refer to PS bit to see wether they can be traversed for the smaller pages within
-		10-11					0 means segfault, 1 means map, 2 means reserve, 3 means swapped
+		56					if set it means the page is taken, for directories refer to PS bit to see wether they can be traversed for the smaller pages within
+		9-11					0 means segfault, 1 means map, 2 means reserve, 3 means swapped, 4 means file
 		57-62					virtual disk number for swapped pages, reserved (ramdisk) in all other cases
 		on file mapping
 					13-45 hold an index into the process' descriptors		//hard maximum of 4mill descriptors
-					46-55 hold the idnex into the pages				//max... a lot of pages? too many to allocate in one go actually
+					48-62 hold the idnex into the pages				//max... a lot of pages? too many to allocate in one go actually
 
 
 Due to the need for the top bits we do 4level paging, nobody has that much memory anyway
@@ -53,18 +53,10 @@ Due to the need for the top bits we do 4level paging, nobody has that much memor
 
 enum pag{ LEV4,BIGPAGE,MIDPAGE,SMALLPAGE};
 class Kingmem{
-	/*
-	recurse directory, if you dont find what you need move on to the next
-	the state you need for the downwards recursion:
-		quota, stack of offsets, stack of tables(&), the success condition, fail on last offset of last table in highest level dir fails
-	*/
-
-//the deal is that instead of passing the pagetree poitner you should have been passing the entire process all along.
-
 	ulong_t offset_by_depth(Process * process, ustd_t pagetype){
 		if !(pagetype){ return process->pagetree;}
 		ulong_t ret = 512*8;
-		ustd_t h = process->code->expected_memory_usage;
+		ustd_t h = 512;
 		for (ustd_t i = 1; i < pagetype; ++i){
 			ret += h;
 			h *= 512;
@@ -72,80 +64,67 @@ class Kingmem{
 		return ret;
 	}
 	void * vmtree_fetch(Process * process, ustd_t pages_number, ustd_t pagetype){
-		process->stream_init(void);
-		ulong_t * tables[pag::SMALLPAGE-pagetype+1];
-		tables[0] = process->pagetree;
-		void * offsets = NULL;
-		ustd_t quota = 1;
+		process->stream_init(NUH);
 
-		memset(&tables[1],0,4*(pag::SMALLPAGE-pagetype));
-		memset(offsets,0,4*(pag::SMALLPAGE-pagetype));
+		ustd_t iterator = 0;
+		ustd_t endcond = gte_multi_from_pagetype(pagetype)/4096;
+		uint64_t * pointer = offset_by_depth(process,pagetype);
 
-		for (ustd_t su = 0; su < pagetype; ++su);
-			ustd_t boo = 0;
-			for (ustd_t h = 0; h < 512; ++h){
-				++boo;
-				if ((tables[su][h] & 512)&&(pagetype == pag::SMALLPAGE))||((pagetype != pag::SMALLPAGE)&&(!(tables[su][h]] & 128))){//LOL
-					 boo = 0;
-				}
-				if !(tables[su][h] & 1){ --boo;}	//we have a bunch of stacks so i am not stopping it instantly
-				if (boo = quota){ break;}
+		for (1; iterator < endcond; ++iterator){				//do we need argumented recursion or is this going to be fine? i think it is offsets fine.
+			ustd_t j;
+			for (j = 0; j < pages_number; +++j){
+				if !(iterator%512){break;}				//bail on directory boundary
+				if !(pointer[iterator] & 1){break;}			//bail if the entry is marked as unusable
+				if (pointer[iterator] & 1<<56){break;}			//bail if an entry is occupied
 			}
-			if (boo == quota){
-				tables[su+1] = offset_by_depth(get_process_object(void),su+1) + offsets<<(64-(12+9*(pag::SMALLPAGE-su)>>(12+9*(pag::SMALLPAGE-su)));
+			if !(j == pages_number){ continue;}
+
+			if (vmto_entry(process->pagetree,ret) == &pointer[iterator]){	//checking wether any fathering directories were signed, they are signed in vmto_entry...
+				__non_temporal process->calendar = 0;
+				return entryto_vm(process->pagetree,&pointer[iterator]);
 			}
-			else{
-				do {
-					offsets |= 1<<(12+(pag::SMALLPAGE-su)*9);
-					ustd_t shift = 12+9*(pag::SMALLPAGE-su);
-					if (offsets<<(64-shift)>>57 == 511){ --su;}
-					else {break;}
-				}while (su);
-				if !(su){
-					__non_temporal process->calendar = 0;
-					return -1;
-				}
-			}
-			if (su+1 == pagetype){ quota = pages_number;}
-		}
-		//signing directories upwards
-		for (ustd_t k = 0; k < pagetype; ++k){
-			tables[k][offsets<<(12+su*9)>>(64-(12+(pag::SMALLPAGE-k)*9))] |= 1024;
 		}
 		__non_temporal process->calendar = 0;
-		return offsets;
+		return NULLPTR;
 	}
-	void vmtree_lay(ulong_t * location, uchar_t expected_usage){	//in gigs
-		void * default_entry = memreq_template(pag::BIGPAGE,mode::RESERVE,cache::WRITEBACK);
+	void vmtree_lay(ulong_t * location){	//in gigs
+		void * default_entry = memreq_template(pag::BIGPAGE,mode::SEGFAULT,cache::WRITEBACK);
+		ustd_t level_length = 512*sizeof(void *);
 
-		ustd_t level_len = 512;
-		ulong_t pointer = location+512*8;
-		for (ustd_t level = 0; level < pag::SMALLPAGE; ++level){
-			for (ustd_t h = 0; h < level_len; ++h){
-				location[h] = default_entry | (pointer<<13);
-			}
-			level_len = expected_usage*8;
-			pointer += level_len;
+		uint64_t something = location+level_length;
+		for (ustd_t i = 0; i < expected_usage; ++i){				//signing the toplevel we want to use
+			location[i] = default_entry|((something+i*512)<<13);
 		}
-		for (ustd_t h = 0; h < level_len; ++h){
-			location[h] = default_entry | (pointer);
+		for (ustd_t i = expected_usage; i < 512-expected_usage; ++i){		//signing useless pages as out-of-memory
+			location[i] = default_entry|((something+i*512)<<13);
+		}
+
+		for (ustd_t g = 0; !(pag::SMALLPAGE<g); ++g){					//signing the rest of the pagetable
+			location = something;
+			level_length *= 512;
+			for (ustd_t i = 0; i < level_length; ++i){
+				location[i] = default_entry)
+				if (g != pag::SMALLPAGE){
+					location[i] |= (something+i*512)<<13;}
+			}
 		}
 	}
-	enum cache{ STRONGUNCACHEABLE,WRITETHROUGH,WRITEBACK,WEAKUNCACHEABLE,WRITEPROTECT,WRITECOMBINING,};	//needed for setting write_combining and write_protect through the mtrrs
+	enum cache{ STRONGUNCACHEABLE,WRITETHROUGH,WRITEBACK,WEAKUNCACHEABLE,WRITEPROTECT,WRITECOMBINING,};
 	void * memreq_template(ustd_t pagetype, ustd_t mode, ustd_t cache, ustd_t exec){
 		if (pagetype != pag::SMALLPAGE){
 			ror32(cache,16);
 			(ushort_t)cache <<=5;
 			rol32(cache,16);
 		}
-		return (mode<<9) | cache | exec<<63;
+		return (mode<<9) | cache | exec<<63 | 1;
 	}
 	ulong_t * vmto_entry(uint64_t * pagetree, void * vm, ustd_t * pagetype){
 		pagetype* = 0;
 		ulong_t * ret = process->pagetree;
 		for (ustd_t g = 1; g < pag::SMALLPAGE; ++g){
-			ustd_t shift = 12+9*g;
-			ret += vm<<(64-shift-9)>>shift * get_multi_from_pagetype(pag::SMALLPAGE)/4096*8;
+			ustd_t shift = 12+9*g - 9;
+			ret += vm<<(64-shift)>>shift * get_multi_from_pagetype(pag::SMALLPAGE-g);
+			ret* |= 1<<56;											//needed for vmtree_fetch, avoiding duplicate code
 			if (ret* & 128){
 				break;}
 		}
@@ -158,7 +137,7 @@ class Kingmem{
 		return (entry*)<<5>>18 + vm%(get_multi_from_pagetype(pagetype));
 	}
 	void * entryto_vm(void * entry){
-		Process * process = get_process_object(void);
+		Process * process = get_process_object(NUH);
 		ustd_t whatever = entry - process->pagetree;
 		void * vm = NULLPTR;
 		for (ustd_t i = pag::SMALLPAGE; i; --i){
@@ -170,13 +149,11 @@ class Kingmem{
 	}
 	//useful for identity mapping stuff, returns the entry into the smallpages tables
 	ulong_t * physto_offset(Process * process, void * phys){
-		//skipping the higher pagetables
 		ulong_t offset = 512*8;
 		for (ustd_t j = 0; j < pag::SMALLPAGE; ++j){
-			offset += (process->code->expected_memory_usage)*offset;
+			offset += 512*offset;		//skipping the higher pagetables
 		}
-		//now computing the offset using the pointer
-		ulong_t second = 0;
+		ulong_t second = 0;								//now computing the offset using the pointer
 		for (ustd_t y = 0; y < pag::SMALLPAGE; ++y){
 			ustd_t g = this->get_multi_from_pagetype(y);
 			second += phys/g(g*4096);
@@ -185,7 +162,7 @@ class Kingmem{
 		return process->pagetree+offset+second;
 	}
 	ustd_t deallocate_vm(Process * process, void ** physrets, void * vm, ustd_t pages_number){
-		process->stream_init(void);
+		process->stream_init(NUH);
 		ustd_t pagetype;
 		ulong_t * entries = vmto_entry(process->pagetree,vm,&pagetype);
 		if !(entries){ __non_temporal process->calendar = 0; return 1;}
@@ -206,12 +183,22 @@ class Kingmem{
 			default:{ return 4096;}
 		}
 	}
+	ustd_t get_pagetype_from_number(ustd_t number){
+		number /= 4096;
+		ustd_t pagetype = pag.SMALLPAGE;
+		for (;; number; --pagetype){
+			number /= 512;
+		}
+		++pagetype;
+		if (pagetype > pag.SMALLPAGE){ --pagetype;}
+		return pagetype;
+	}
 	/*
 	Array of bits where each bit stands for a 4096 byte page, we have alignment requirements for the allocation, returned 1 doesnt negate the contents
 	of the returned array meaning that you can get whatever is available (interactive syscalls?)
 	*/
 	ustd_t get_free_phys(void ** physrets, ustd_t pages_number, ustd_t pagetype){
-		this->stream_init(void);
+		this->stream_init(NUH);
 		ustd_t multiplier = get_multi_from_pagetype(pagetype);
 		ustd_t bit = 1;
 		ustd_t gate = 0;
@@ -240,7 +227,7 @@ class Kingmem{
 		return 1;
 	}
 	void * get_free_identity(ustd_t pages_number, ustd_t pagetype){
-		this->stream_init(void);
+		this->stream_init(NUH);
 		ustd_t multiplier = get_multi_from_pagetype(pagetype);
 		ustd_t bit = 1;
 		ustd_t gate = 0;
@@ -266,7 +253,7 @@ class Kingmem{
 	//this being one function is kind of silly...
 	enum actions{ SET,CLEAR};
 	void manipulate_phys(void ** phys, ustd_t pages_number, ustd_t pagetype, ustd_t action){
-		if (action == actions::SET){ this->stream_init(void);}
+		if (action == actions::SET){ this->stream_init(NUH);}
 		ustd_t quota = get_multi_from_pagetype(pagetype);
 		for (ustd_t i = 0; i < pages_number; ++i){
 			ustd_t indexer = phys/8/4096;
@@ -296,12 +283,12 @@ class Kingmem{
 		ulong_t * off = vmto_entry(pagetree,ret,&pagetype);
 		ustd_t addition = get_multi_from_pagetype(pagetype);
 		for (ustd_t h = 0; h < pages_number; ++h){
-			off[h] = templ | ((target+addition*h)<<12);
+			off[h] = templ | ((target+addition*h)<<13);
 		}
 	}
 	void mem_free(void * pagetree, void * vm, ustd_t pages_number, ustd_t pagetype){	//you know that running this on fucking mapped pages marks teh end?
 		void * phys[pages_number];
-		if (deallocate_vm(get_process_object(void),phys,vm,pages_number)){get_process_object(void)->sigset |= signals::SIGSEGV; return;}
+		if (deallocate_vm(get_process_object(NUH),phys,vm,pages_number)){get_process_object(NUH)->sigset |= signals::SIGSEGV; return;}
 		manipulate_phys(phys,pages_number,pagetype,CLEAR);
 		void * templ = memreq_template(pagetype,mode::RESERVE,cache::WRITEBACK);
 		ulong_t off = vmto_entry(vm);
@@ -317,7 +304,7 @@ class Kingmem{
 
 		for (ustd_t h = 0; h < pages_number; ++h){
 			if (off[h] & 4){ return 1;}									//supervisor...
-			if (off[h]<<53>>61 != mode::MAP){ get_process_object(void)->sigset |= SIGSEGV; return 2;}
+			if (off[h]<<53>>61 != mode::MAP){ get_process_object(NUH)->sigset |= SIGSEGV; return 2;}
 		}
 		for (ustd_t h = 0; h < pages_number; ++h){
 			off[h] = templ;			//this is kind of bad though, because it lets you overwrite stuff like gdt or whatever, masssively dangerous.
@@ -327,14 +314,14 @@ class Kingmem{
 		if (oldpages > newpages){return -1;}
 		void * phys[newpages];
 		if (deallocate_vm(pagetree,phys,vm,oldpages)){
-			get_process_object(void)->sigset |= signals::SIGSEGV;}
+			get_process_object(NUH)->sigset |= signals::SIGSEGV;}
 		vm = vmtree_fetch(pagetree,newpages,pagetype);				//reminder to make the top of the possible address space reserved
 		if (vm == -1){ return -2;
 		if (get_free_phys(&phys[oldpages-1],newpages-oldpages,pagetype)){ return -3;}
 		void * templ = memreq_template(pagetype,mode::RESERVE,cache::WRITEBACK);
 		ulong_t off = vmto_entry(pagetree,vm);
 		for (ustd_t h = 0; h < newpages; ++h){
-			off[h] = templ | phys[h]<<12;
+			off[h] = templ | phys[h]<<13;
 		}
 		return vm;
 	}
@@ -342,7 +329,7 @@ class Kingmem{
 		if (oldpages > newpages){return -1;}
 		void * phys[newpages];
 		if (deallocate_vm(pagetree,phys,vm,oldpages)){					//this is going to segmentation fault no matter what you do
-			get_process_object(void)->sigset |= signals::SIGSEGV; return -1;}
+			get_process_object(NUH)->sigset |= signals::SIGSEGV; return -1;}
 		vm = vmtree_fetch(pagetree,newpages,pagetype);
 		if (vm == -1){
 			return -2;}
@@ -350,7 +337,7 @@ class Kingmem{
 		ulong_t off = vmto_entry(pagetree,vm);
 		ustd_t addition = get_multi_from_pagetype(pagetype);
 		for (ustd_t h = 0; h < newpages; ++h){
-			off[h] = templ | (phys+addition*h)<<12;
+			off[h] = templ | (phys+addition*h)<<13;
 		}
 		return vm;
 	}
@@ -364,15 +351,15 @@ class Kingmem{
 	This swaps out to disks whatever pages have been marked as reserved in a process' pagetree
 	*/
 	void swap_process(void){
-		DisksKing * dking = get_disksking_object(void);
-		Process * process = get_process_object(void);
-		Virtual_fs * vfs = get_vfs_object(void);
-		Kingmem * mm = get_kingmem_object(void);
+		DisksKing * dking = get_disksking_object(NUH);
+		Process * process = get_process_object(NUH);
+		Virtual_fs * vfs = get_vfs_object(NUH);
+		Kingmem * mm = get_kingmem_object(NUH);
 
-		process->stream_init(void);
+		process->stream_init(NUH);
 
 		ulong_t * pointer = process->pagetree;
-		ustd_t interval = process->code->expected_memory_usage;
+		ustd_t interval = 512;
 		ustd_t scaling = 512;
 		ustd_t pagetype = 0;
 		for (ustd_t i = 0; 1; ++i){
@@ -384,10 +371,10 @@ class Kingmem{
 			}
 			if !(mm->sizeof_ram/mm->used_memory < 2){ break;}			//
 			if (scaling == 512*512*512){						//the swapping conditions
-				if !(pointer[i] & 512){
+				if !(pointer[i] & 1<<56){
 					continue;}
 			}else{
-				if !(pointer[]i & 128){
+				if !(pointer[i] & 128){
 					continue;}
 			}									//
 												//the swapping code
@@ -403,9 +390,9 @@ class Kingmem{
 		__non_temporal process->calendar = 0;
 	}
 	__attribute__((interrupt)) void fault_handler(void){
-		enter_ringzero(void);
-		Process * process = get_process_object(void);
-		Kingmem * mm = get_kingmem_object(void);
+		enter_ringzero(NUH);
+		Process * process = get_process_object(NUH);
+		Kingmem * mm = get_kingmem_object(NUH);
 		void * vm = __asm__("mov %%cr2,%%rax\n\t");
 		ustd_t pagetype;
 		ulong_t * entry = mm->vmto_entry(process->pagetree,vm,&pagetype);		//MASSIVE DANGER read the binary for how pagetype gets passed, if shit is grim use Kingmem (this is locked anyway)
@@ -425,12 +412,12 @@ class Kingmem{
 				void * phys = malloc(1,pagetype);
 				entry* |= phys;
 			break;}
-			case mode::MAP:{
+			case mode::FILE:{				//see SYS_FMAP
 				ustd_t desc = (entry*)<<18>>32;
-				ustd_t page = (entry*)>>46;
+				ustd_t page = (entry*)<<1>>(48+1);
 
 				File * file = &vfs->descriptions[process->descs->pool[desc]->findex];
-				entry* = vfs->load_page(get_disksking_object(void),file,page) | memreq_template(pagetype,mode::MAP,file->cache,file->executable);
+				entry* = vfs->load_page(get_disksking_object(NUH),file,page) | memreq_template(pagetype,mode::MAP,file->cache,file->executable);
 			break;}
 			case mode::SEGFAULT:{
 				process->sigset |= signals::SIGSEGV;
@@ -441,16 +428,49 @@ class Kingmem{
 		if (pagetype != pag::SMALLPAGE){++bit;}
 		if ((entry*<<(64-12))>>54 == mode::SWAPPED){
 			void * phys = malloc(1,pagetype);
-			void * newentry = this->vmtree_fetch(process->pagetree,1,pagetype);
-			newentry = this->memreq_template(pagetype,RESERVE,WRITEBACK,0) | (phys<<5>>18);	//userspace means writeback is fit.
-			ustd_t disk = (entry*)>>57;
-			void * doffset = (entry*) <<5>>18;
-			DisksKing * dking = get_disksking_object(void);
-			dking->read(disk,doffset,phys,1,pagetype);
-			ustd_t pages = this->get_multi_from_pagetype(pagetype) / 4096;
-			Virtual_fs * vfs = get_vfs_object(void);
-			vfs->descriptions[dking->disks[disk]]->shm->pool_free(pages);
+			ustd_t ct = entry*<<1>>(57+1);
+			Disksking * dking = get_disksking_object(NUH);
+			for (ustd_t i = 0; i < dking->partitions->length; ++i)}
+				if (dking->partitions->ckarray[i] == 0){ continue;}
+				if (dking->partitions->pool[i]->signature == SWAP){
+					--ct;
+				}
+				if (ct == 0){ dking->raw_read(entry*<<1>>(57+1)),entry*<<17>>30;
+				break;}
+			}
+			if (ct){ free(phys,1,pagetype);
+				process->sigset |= SIGKILL;
+				return;
+			}
+
+			entry* = this->memreq_template(pagetype,RESERVE,WRITEBACK,0) | (phys<<5>>18);	//userspace means writeback is fit.
 		}
 		else{process->sigset |= SIGSEGV;}
+	}
+	void recursive_free(Process * process, ustd_t pagetype, uint64_t * pointer){
+		for (ustd_t i = 0; i < 512; ++i){
+			if !(pointer[i] & 1<<56){ continue;}				//if the page was not in use ignoring
+
+			switch (pointer[i]<<(64-9)>>55){				//switch statement on the modes..., map gets ignored because whatever
+				case SEGFAULT:{ break;}
+				case RESERVE:{
+					mem_free(process->pagetree,entryto_vm(process->pagetree,&pointer[i],1,pagetype));
+				break;}
+				case FILE:{
+					Virtual_fs * vfs = get_vfs_object(NUH);
+					File * file = &vfs->descriptions[process->descs->pool[pointer[i]<<1>>(48+1)]->findex];
+					void ** dub = file->mem->pool;
+					void * comp = pointer[i]<<17>>30;
+					for (ustd_t k = 0; k < file->mem->length; ++k){	//sorting for which page we are even pointing at, not enough bits...
+						if (dub* == comp){
+							--file->mem->ckarray[k];
+						break;}
+					}
+				break;}
+			}
+			if (pagetype == pag::SMALLPAGE){continue;}			//ignoring directory checks...
+			if !(pointer[i] & 128){						//calling recursion on not-empty page directory
+				recursive_free(process,pagetype+1,&pointer[i]);}
+		}
 	}
 };
