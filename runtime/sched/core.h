5cbb7e598277b274d26bbedbc2825f022d317de7
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-06 18:56:59 +0000

    Update core.h

--- runtime/sched/core.h
+++ runtime/sched/core.h
@@ -1,6 +1,7 @@
 class Descriptor{
 	ustd_t findex;
 	ustd_t flags;
+	uint64_t stream_offset;
 };
 class process_descriptors : Hash{
 	Descriptor ** pool : Hash.pool;
@@ -15,32 +16,50 @@ class local_descriptor_table : King{
 	ustd_t segment_selector;
 };
 
+struct state{
+	uint64_t accumulator;
+	uint64_t counter;
+	uint64_t rdx;
+	uint64_t rbx;
+	uint64_t stack_pointer;
+	uint64_t string_dest;
+	uint64_t string_source;
+	uint64_t frame_pointer;
+	uint64_t eight;
+	uint64_t nine;
+	uint64_t ten;
+	uint64_t eleven;
+	uint64_t twelve;
+	uint64_t thirteen;
+	uint64_t fourteen;
+	uint64_t fifteen;
+	uint64_t flags;
+	uint64_t instruction_pointer;
+	uint64_t fs;
+	uint64_t ds;
+};
 enum thread_types{ DRIVER,MODULE,THREAD,DEVICE,KERNEL,};	//all have different behaviour
 class Thread : King{
 	Process * parent;
 	State state;			//this includes, at the start, rip,cs,rsp,ss,rflags from the tss and then also all general purpose and vectors
 	ustd_t type;
-	auto * double_link;		//see type
+	auto * double_link;		//ergo. getting teh device structure inside of the input system calls...
 	sig_t sigmask;
 	sig_t sigset;
 	(void (*)(void * data))[MAXSIG];
 //	ustd_t stacksize;		//process-based
 	ustd_t taken;
-	ulong_t syscall_returnval;	//we execute them instantly and then schedule...
+	ulong_t sys_retval;		//we execute them instantly and then schedule...
 	pollfd * poll;			//physical pointer to the userspace array of polling structures and their number
 	ustd_t pollnum;
 	Thread * prior;			//see ioctl
 	ulong_t target_micros;		//see sys_sleep
+	void * heap;
 
 	void stream_init(void){
-		volatile __non_temporal while (this->taken);
-
-		brothers_sleep(void);
-		__non_temporal this->taken = 255;
-		brothers_wake(void);
+		mutex(&this->taken);
         }
 };
-
 class Process : King{
 	Thread ** workers : King.pool;
 	user_t owner_id;
@@ -64,7 +83,7 @@ class Processor{
 enum polling_actions{ READING,WRITING,MUTEX,};		//... , ... , waiting for no other processes to have the file open			may be ORd
 struct pollfd{
 	ustd_t fdesc;
-	ushort_t action;
+	ushort_t reqaction;
 	ushort_t retaction;
 };
 
