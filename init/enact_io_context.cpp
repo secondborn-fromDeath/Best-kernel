d398ebd81a5e7749f50b36a65e4ec7c6ad133f65
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-06 18:51:53 +0000

    Update enact_io_context.cpp

--- init/enact_io_context.cpp
+++ init/enact_io_context.cpp
@@ -2,6 +2,8 @@
 Creating the IO context*/
 using enum device_driver_routines;
 void enact_IO_context(ustd_t drivers_number, image_loaded ** driver_pointers, image_loaded * info_pointers){
+	highprio_brothers_sleep(NUH);
+
 	Kingprocess * kprc = get_kingprocess_object(NUH);
 	Virtual_fs * vfs = get_vfs_object(NUH);
 	Kingthread * ktrd = get_threadking_object(NUH);
@@ -12,10 +14,10 @@ void enact_IO_context(ustd_t drivers_number, image_loaded ** driver_pointers, im
 	kernel->prior = ori_thread;
 	for (ustd_t i = 0; i < drivers_number; ++i){
 		++drivers_number;
-		Storage * fdriv = vfs->ramcontents_to_description(driver_pointers[drivers_number]->image_pointer,driver_pointers[drivers_number]->image_size,get_pagetype_from_number(driver_pointers[drivers_number]->image_size));
+		fiDriv * fdriv = vfs->ramcontents_to_description(driver_pointers[i]->image_pointer,driver_pointers[i]->image_size,get_pagetype_from_number(driver_pointers[i]->image_size));
 		drivgod->pool_alloc(1)* = fdriv;
 		fdriv->d->runtime = kprc->pool[exec(fdriv)];
-		fdriv->d->runtime->code->models = vfs->ramcontents_to_description(driver_pointers[drivers_number]->image_pointer,driver_pointers[drivers_number]->image_size,get_pagetype_from_number(info_pointers[drivers_number]->image_size));
+		fdriv->d->runtime->code->models = vfs->ramcontents_to_description(info_pointers[i]->image_pointer,driver_pointers[i]->image_size,get_pagetype_from_number(info_pointers[i]->image_size));
 		memcpy(&fdriv->meta.name,&fdriv->d->runtime->code->name,sizeof(File.meta.name));
 		module_ctl(fdriv,GET_MODE_IRHANDLER,NULLPTR);
 	}
