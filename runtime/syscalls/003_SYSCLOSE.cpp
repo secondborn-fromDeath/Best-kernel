9ecfc26a65c3765474fa0ce550b4899e7ce0869a
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-06 19:02:52 +0000

    Update 003_SYSCLOSE.cpp

--- runtime/syscalls/003_SYSCLOSE.cpp
+++ runtime/syscalls/003_SYSCLOSE.cpp
@@ -1,11 +1,12 @@
 void SYSCLOSE(ustd_t desc){
-	Thread * thread = get_thread_object(void);
+	Thread * thread = get_thread_object(NUH);
 	Process * process = thread->parent;
 
 	CONDITIONAL_SYSRET(thread,(!(process->descs->count > desc)||(process->descs->ckarray[desc] == 0)),-1);
 
 	process->descs->pool_free(&process->descs->pool[desc],1);
-	--get_vfs_object(void)->descriptions[process->descs->pool[desc]->findex]->listeners;
+	Virtual_fs * vfs = get_vfs_object(NUH);
+	--vfs->descriptions[process->descs->pool[desc]->findex]->listeners;
 	thread->sys_retval = 0;
 	SYSRET;
 }
