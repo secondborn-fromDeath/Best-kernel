bed8b5f7e6f9be59a7fc76d57e3c2b6127b02734
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-06 20:32:43 +0000

    Update 020_SYSFMAP.cpp

--- runtime/syscalls/020_SYSFMAP.cpp
+++ runtime/syscalls/020_SYSFMAP.cpp
@@ -1,6 +1,6 @@
 //amount and offset are absolute because the mapped_pagetype for the file is transparent to the process
 void * fmap(Process * process, File * file, ustd_t desc, ustd_t amount, ustd_t offset){
-	Kingmem * mm = get_kingmem_object(void);
+	Kingmem * mm = get_kingmem_object(NUH);
 
 	ustd_t pages_number = pagetype_roundup(file->mem->mapped_pagetype,amount);
 	ustd_t multi = get_multi_from_pagetype(file->mem->mapped_pagetype);
@@ -21,14 +21,27 @@ void * fmap(Process * process, File * file, ustd_t desc, ustd_t amount, ustd_t o
 }
 
 void SYS_FMAP(ustd_t desc, ustd_t amount, ustd_t offset){
-	Thread * thread = get_thread_object(void);
+	Thread * thread = get_thread_object(NUH);
 	Process * process = thread->parent;
+	CONDITIONAL_SYSRET(thread,desc > 1<<14,-3);										//see the top of mem/core.h
 	CONDITIONAL_SYSRET(thread,(!(process->descs->count > desc)||(process->descs->ckarray[desc] == 0)),-1);
 
-	Virtual_fs * vfs = get_vfs_object(void);
+	Virtual_fs * vfs = get_vfs_object(NUH);
 
 	File * file = &vfs->descriptions[process->descs->pool[desc]->findex];
 	CONDITIONAL_SYSRET(thread,((file->meta.mode & permissions::WRITE)&&(process->owner_id != owner_ids::ROOT)),-2);
+
+	char ** maps = &process->descs->pool[desc]->maps;
+	if (maps* == 0){
+		maps* = kptralloc(512/8);
+	}
+	ustd_t pureoff = offset/get_multi_from_pagetype(file->mem->mapped_pagetype);
+	for (ustd_t g = 0; g < pagetype_roundup(amount,file->mem->mapped_pagetype); ++g){
+		if (maps*[g+pureoff] == 1){
+			process->sigset |= signals::SIGKILL;
+		}
+		maps*[g+pureoff] = 1;
+	}
 
 	thread->sys_retval = fmap(process,file,amount,offset);
 	SYSRET;
