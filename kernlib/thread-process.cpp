dbe2c0484d48a783e9be02335fba193df0300f34
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-07 21:58:30 +0000

    Update thread-process.cpp

--- kernlib/thread-process.cpp
+++ kernlib/thread-process.cpp
@@ -1,12 +1,28 @@
-Thread * get_thread_object(void){
+volatile Thread * get_thread_object(void){
 	Thread * ret;
-	__asm__(
+	__asm__ volatile(
 	"MOVq	%%cr11,%%rax\n\t"
-	::"r"(ret):);
+	:"=r"(ret)::);
+	return ret;volatile Thread * get_thread_object(void){
+	Thread * ret;
+	__asm__ volatile(
+	"MOVq	%%cr11,%%rax\n\t"
+	:"=r"(ret)::);
 	return ret;
 }
 void set_thread_object(Thread * pointer){
-	__asm__(
+	__asm__ volatile(
+	"MOVq	%%cr11,%%rax\n\t"
+	::"r"(pointer):);
+}
+Process * get_calling_process(void){
+	Thread * thread = get_thread_object(NUH);
+	return thread->parent;
+}
+
+}
+void set_thread_object(Thread * pointer){
+	__asm__ volatile(
 	"MOVq	%%cr11,%%rax\n\t"
 	::"r"(pointer):);
 }
