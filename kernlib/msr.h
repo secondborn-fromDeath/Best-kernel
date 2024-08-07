d50031aad88d61fcbb8ee7f4f4ab8ab837330e64
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-07 21:48:06 +0000

    Update msr.h

--- kernlib/msr.h
+++ kernlib/msr.h
@@ -5,6 +5,7 @@
 }
 #define rdmsr(c,a,d){
 	__asm__ volatile(
-	"WRMSR\n\t"
-	::"r"(a),"r"(c),"r"(d):);	//c is register selector, a is bottom, d is top
+	"MOVQ	%%rax,%%rcx\n\t"
+	"RDMSR\n\t"
+	:"=r"(a),"=r"(d):"r"(c):);	//c is register selector, a is bottom, d is top
 }
