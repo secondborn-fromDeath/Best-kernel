da518454205d2390136154ca7d3eb217c8081f7b
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-07 21:51:01 +0000

    Update ports.h

--- kernlib/ports.h
+++ kernlib/ports.h
@@ -13,32 +13,32 @@ void outw(uint16_t data, uint16_t port){
 void outd(uint32_t data, uint16_t port){
 	__asm__(
 	"MOVq	%%ecx,%%edx\n\t"
-	"OUTd	%%eax,%%edx\n\t"
+	"OUTd	%%eax,%%dx\n\t"
 	::"r"(data),"r"(port):);
 }
 
 uint8_t inb(uint16_t port){
 	uint8_t data;
 	__asm__(
-	"MOVq	%%ecx,%%edx\n\t"
-	"INb	%%al,%%dx\n\t"
-	::"r"(data),"r"(port):);
+	"MOVq	%%eax,%%edx\n\t"
+	"INb	%%dx,%%al\n\t"
+	:"=r"(data):"r"(port):);
 	return data;
 }
 uint16_t inw(uint16_t port){
 	uint16_t data;
 	__asm__(
-	"MOVq	%%ecx,%%edx\n\t"
-	"INw	%%ax,%%dx\n\t"
-	::"r"(data),"r"(port):);
+	"MOVq	%%eax,%%edx\n\t"
+	"INw	%%dx,%%ax\n\t"
+	:"=r"(data):"r"(port):);
 	return data;
 }
 uint32_t ind(uint16_t port){
 	uint32_t data;
 	__asm__(
-	"MOVq	%%ecx,%%edx\n\t"
-	"INb	%%eax,%%edx\n\t"
-	::"r"(data),"r"(port):);
+	"MOVq	%%eax,%%edx\n\t"
+	"INb	%%dx,%%eax\n\t"
+	:"=r"(data):"r"(port):);
 	return data;
 }
 
