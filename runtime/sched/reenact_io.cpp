a740f66c0d64a37675963a5fb9a6443261918a62
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-07 22:07:09 +0000

    Update reenact_io.cpp

--- runtime/sched/reenact_io.cpp
+++ runtime/sched/reenact_io.cpp
@@ -1,5 +1,5 @@
 /*
-This gets called every 5 seconds or so from the scheduler code*/
+This gets called every 5 seconds or so from the scheduler code,	NOTE optimization: use an array for the start pointers and make the fixed segemnt size max16bit*/
 void reenact_io(void){
 	Virtual_fs * vfs = get_vfs_object(NUH);
 	Directory * dev = &vfs->descriptions[1];
@@ -11,10 +11,10 @@ void reenact_io(void){
 		dev->children->pool[i]->stream_init(NUH);
 	}
 	highprio_brothers_sleep(NUH);
-	for (ustd_t i = 0; i < modrm_sp->length; ++i){					//removing all of the scheduler drivers
+	for (ustd_t i = 0; i < modrm_sp->length; ++i){					//removing all of the scheduled drivers
 		if !(modrm_sp->ckarray[i]){
 			continue;}
-		modrm(modrm_sp->pool[i]);
+		DRIVKILL(modrm_sp->pool[i]);
 	}
 	Kingmem * mm = get_kingmem_object(NUH);						//using mm->gdt->pool because identity mapping
 	Kingprocess * kprc = get_kingprocess_object(NUH);				//extracting pointers to the interrupt handler segments from every single process
