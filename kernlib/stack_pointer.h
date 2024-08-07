9f0a2e0628fe7861f92acbaa7fe0399ec843edf6
Mirukaj Gabriel <laymanvonton@gmail.com>
2024-08-07 21:56:52 +0000

    Update stack_pointer.h

--- kernlib/stack_pointer.h
+++ kernlib/stack_pointer.h
@@ -1,7 +1,7 @@
 auto * get_stack_pointer(void){
 	auto * ret;
-	__asm__("MOVq	%%rsp,%%rax\n\t" ::"r"(ret):);
+	__asm__ volatile("MOVq	%%rsp,%%rax\n\t" :"=r"(ret)::);
 }
 void set_stack_pointer(auto * newstack){
-	__asm__("MOVq	%%rax,%%rsp\n\t" ::"r"(newstack):);	//should be fine...
+	__asm__ volatile("MOVq	%%rax,%%rsp\n\t" ::"r"(newstack):);	//should be fine...
 }
